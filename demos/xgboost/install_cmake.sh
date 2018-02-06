3b8e3e653a6bb8204850ebd8b470abec1883700d
yuanwu <yuan.wu@intel.com>
2018-02-06 19:01:29 +0000

    Update the xgboost to latest version
    Intel has optimized the xgboost in latest version. Performance has
    obvious acceleration in benchmark. In svm training with Yahoo LTR data,
    latest version gets 7.8X acceleration in host, and 2X acceleration in occlum.
    
    Signed-off-by: yuanwu <yuan.wu@intel.com>

---
+++ demos/xgboost/install_cmake.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+cd ~
+wget https://github.com/Kitware/CMake/releases/download/v3.15.5/cmake-3.15.5.tar.gz && tar xf cmake-3.15.5.tar.gz
+cd cmake-3.15.5
+./bootstrap
+make -j4
+sudo make install
