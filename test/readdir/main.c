011b4bf8e792df32668c0c204fb4492a2c09a699
WangRunji <wangrunji0408@163.com>
2019-03-13 17:32:35 +0800

    add test for readdir

---
+++ test/readdir/main.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+    DIR* dirp = opendir(".");
+    if (dirp == NULL) {
+        printf("failed to open directory at /\n");
+        return -1;
+    }
+
+	struct dirent *dp;
+	while ((dp = readdir(dirp)) != NULL) {
+		printf("get: %s\n", dp->d_name);
+	}
+
+    closedir(dirp);
+
+    printf("Read directory test successful\n");
+    return 0;
+}
