ba7db98e49372c9b178f353bc5b7babfbc49c98e
LI Qing <geding.lq@alibaba-inc.com>
2019-08-19 14:44:45 +0800

    Add Tensorflow Lite Demo
    The demo shows how to run the unmodified Tensorflow Lite on Occlum.

---
+++ demo/tensorflow_lite/run_tflite_in_linux.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/usr/local/occlum/x86_64-linux-musl/lib:$LD_LIBRARY_PATH
+set -e
+
+show_usage() {
+    echo
+    echo "Usage: $0 demo/benchmark"
+    echo
+}
+
+copy_files() {
+    cp -f tensorflow_src/tensorflow/lite/tools/make/gen/linux_x86_64/bin/* .
+    cp -rf tensorflow_src/tensorflow/lite/examples/label_image/testdata .
+}
+
+run_demo() {
+    copy_files
+    ./label_image \
+        --tflite_model ./models/mobilenet_v1_1.0_224.tflite \
+        --labels ./models/labels.txt \
+        --image ./testdata/grace_hopper.bmp
+}
+
+run_benchmark() {
+    copy_files
+    ./benchmark_model \
+        --graph=./models/mobilenet_v1_1.0_224.tflite \
+        --warmup_runs=5
+}
+
+bin=$1
+case "$bin" in
+    demo)
+        run_demo
+        ;;
+    benchmark)
+        run_benchmark
+        ;;
+    *)
+        show_usage
+esac
