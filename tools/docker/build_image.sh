449ba1d5d986ca4eab8d5a7537541c72097b9fda
LI Qing <geding.lq@alibaba-inc.com>
2020-01-14 13:28:29 +0800

    Add Dockerfile for Ubuntu 18.04

--- tools/docker/build_image.sh
+++ tools/docker/build_image.sh
@@ -15,11 +15,12 @@ USAGE:
     build_image.sh <OCCLUM_LABEL> <OS_NAME>
 
 <OCCLUM_LABEL>:
-    An arbitrary string chosen by the user to describe the version of Occlum preinstalled in the Docker image, e.g., "latest", "0.8.0", "prerelease", and etc.
+    An arbitrary string chosen by the user to describe the version of Occlum preinstalled in the Docker image, e.g., "latest", "0.9.0", "prerelease", and etc.
 
 <OS_NAME>:
     The name of the OS distribution that the Docker image is based on. Currently, <OS_NAME> must be one of the following values:
         ubuntu16.04         Use Ubuntu 16.04 as the base image
+        ubuntu18.04         Use Ubuntu 18.04 as the base image
         centos7.2           Use CentOS 7.2 as the base image
 
 The resulting Docker image will have "occlum/occlum:<OCCLUM_LABEL>-<OS_NAME>" as its label.
@@ -36,9 +37,13 @@ fi
 occlum_label=$1
 os_name=$2
 
-if [ "$os_name" != "ubuntu16.04" ] && [ "$os_name" != "centos7.2" ];then
-    report_error
-fi
+function check_item_in_list() {
+    item=$1
+    list=$2
+    [[ $list =~ (^|[[:space:]])$item($|[[:space:]]) ]]
+}
+
+check_item_in_list "$os_name" "ubuntu16.04 ubuntu18.04 centos7.2" || report_error
 
 cd "$script_dir/.."
 docker build -f "$script_dir/Dockerfile.$os_name" -t "occlum/occlum:$occlum_label-$os_name" .
