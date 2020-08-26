027e1569e063b106afe4e669b079dd41a4e4bd9a
Hui, Chunyang <sanqian.hcy@antfin.com>
2020-08-26 17:06:44 +0000

    Fix tensorflow lite demo eigen download failure

--- demos/tensorflow_lite/download_and_build_tflite.sh
+++ demos/tensorflow_lite/download_and_build_tflite.sh
@@ -8,6 +8,8 @@ pushd $SRC_DIR
 git clone https://github.com/tensorflow/tensorflow .
 git checkout tags/v1.15.0-rc0 -b v1.15.0-rc0
 git apply ../patch/fix-tflite-Makefile-v1.15.0-rc0.diff
+# Fix eigen source package url failure
+sed -i '32 s/.*/EIGEN_URL="https:\/\/storage.googleapis.com\/mirror.tensorflow.org\/bitbucket.org\/eigen\/eigen\/get\/49177915a14a.tar.gz"/' tensorflow/lite/tools/make/download_dependencies.sh
 ./tensorflow/lite/tools/make/download_dependencies.sh
 make -j 3 -f tensorflow/lite/tools/make/Makefile
 popd
