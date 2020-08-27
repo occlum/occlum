db9f1d79db26186ff44531642a48c01fb5fb83a3
James Dong <james.dong.security@gmail.com>
2020-08-27 17:22:40 +0000

    Add a sample of Golang gRPC client/server programs

---
+++ demos/golang/grpc_pingpong/go.mod
@@ -0,0 +1,9 @@
+module grpc_pingpong
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.4.1
+	google.golang.org/grpc v1.33.0-dev
+	google.golang.org/protobuf v1.25.0
+)
---
+++ demos/golang/grpc_pingpong/prepare_ping_pong.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+# sanity check
+FILE_SET="
+occlum_ping
+occlum_pong
+go.sum"
+for CURR_FILE in $FILE_SET
+do
+        if [ -f "$CURR_FILE" ]; then
+                rm $CURR_FILE
+        fi
+done
+
+DIR_SET="
+github.com
+occlum_ping_instance
+occlum_pong_instance"
+for CURR_DIR in $DIR_SET
+do
+        if [ -d "$CURR_DIR" ]; then
+                rm -fr $CURR_DIR
+        fi
+done
+
+# enable Go modules for package management 
+export GO111MODULE=on
+
+# update PATH so that the protoc compiler can find the plugin:
+export PATH="$PATH:$(go env GOPATH)/bin"
+
+# assume that protoc is installed
+if ! type "protoc" > /dev/null 2>&1; then
+        echo "Please install protoc"
+        exit 1
+fi
+
+# install protoc-gen-go and protoc-gen-go-grpc plugin
+if ! type "protoc-gen-go" > /dev/null 2>&1; then
+        go get google.golang.org/protobuf/cmd/protoc-gen-go
+fi
+if ! type "protoc-gen-go-grpc" > /dev/null 2>&1; then
+        go get google.golang.org/grpc/cmd/protoc-gen-go-grpc
+fi
+
+# compiling pingpong gRPC .proto file
+export GOPRIVATE=github.com/occlum/demos/\*
+protoc --proto_path=pingpong --go-grpc_out=. --go_out=. pingpong/pingpong.proto
+
+# prepare occlum images
+occlum-go build -o occlum_pong pong.go
+occlum-go build -o occlum_ping ping.go
