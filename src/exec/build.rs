9c390923a50d74c54655d8158a98d6201562877c
He Sun <bochang.sh@antgroup.com>
2020-09-16 15:25:39 +0800

    Narrow the focus of exec client/server build
    Only change of the proto file should trigger the build script.

--- src/exec/build.rs
+++ src/exec/build.rs
@@ -1,13 +1,16 @@
 extern crate protoc_rust_grpc;
 
+const PROTO_FILE: &str = "occlum_exec.proto";
+
 fn main() {
     protoc_rust_grpc::Codegen::new()
         .out_dir("src")
-        .input("occlum_exec.proto")
+        .input(PROTO_FILE)
         .rust_protobuf(true)
         .run()
         .expect("protoc-rust-grpc");
 
+    println!("cargo:rerun-if-changed={}", PROTO_FILE);
     println!("cargo:rustc-link-search=native=../../build/lib");
     println!("cargo:rustc-link-lib=dylib=occlum-pal");
 }
