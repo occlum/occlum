a5c2e553b72961dade5a11a6f3c57d51c2970e6b
zongmin.gu <zongmin.gzm@antgroup.com>
2020-10-21 16:08:21 +0800

    Bump version to 0.17.0

--- src/pal/include/occlum_version.h
+++ src/pal/include/occlum_version.h
@@ -1,9 +1,9 @@
 #ifndef _OCCLUM_VERSION_H_
 #define _OCCLUM_VERSION_H_
 
-// Version = 0.16.0
+// Version = 0.17.0
 #define OCCLUM_MAJOR_VERSION    0
-#define OCCLUM_MINOR_VERSION    16
+#define OCCLUM_MINOR_VERSION    17
 #define OCCLUM_PATCH_VERSION    0
 
 #define STRINGIZE_PRE(X) #X
