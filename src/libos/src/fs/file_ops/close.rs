83ce318f6c803d9586946c458bfbcc51e51b8f7b
Tate, Hongliang Tian <tate.thl@antfin.com>
2020-11-18 10:48:31 +0000

    Unregister a file from epoll files when the file is closed
    Usually, files are unregistered from an epoll file via the EPOLL_CTL_DEL command
    explicitly. But for the sake of users' convenience, Linux supports
    unregistering a file automatically from the epoll files that monitor the file
    when the file is closed. This commit adds this capability.

--- src/libos/src/fs/file_ops/close.rs
+++ src/libos/src/fs/file_ops/close.rs
@@ -4,6 +4,12 @@ pub fn do_close(fd: FileDesc) -> Result<()> {
     debug!("close: fd: {}", fd);
     let current = current!();
     let mut files = current.files().lock().unwrap();
-    files.del(fd)?;
+    let file = files.del(fd)?;
+    // Deadlock note: EpollFile's drop method needs to access file table. So
+    // if the drop method is invoked inside the del method, then there will be
+    // a deadlock.
+    // TODO: make FileTable a struct of internal mutability to avoid deadlock.
+    drop(files);
+    drop(file);
     Ok(())
 }
