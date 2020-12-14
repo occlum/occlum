7f5cdb9f89ba0bc6ae8452aa555543cd9947a2ec
He Sun <bochang.sh@antgroup.com>
2020-12-14 05:04:52 +0000

    Add support for DCAP
    1. Five new ioctl commands of /dev/sgx are added for occlum
    applications to securely get and verify DCAP quote;
    2. Not all the functions of the intel DCAP package are open to
    developers to simplify the DCAP usage;
    3. The test may only run on the platform with DCAP driver installed;
    4. A macro OCCLUM_DISABLE_DCAP is used to separate the DCAP code from
    the other code.

--- src/libos/src/fs/dev_fs/dev_sgx/consts.rs
+++ src/libos/src/fs/dev_fs/dev_sgx/consts.rs
@@ -3,6 +3,7 @@ use super::*;
 /// Ioctl to check if EDMM (Enclave Dynamic Memory Management) is supported
 pub const SGX_CMD_NUM_IS_EDMM_SUPPORTED: u32 =
     StructuredIoctlNum::new::<i32>(0, SGX_MAGIC_CHAR, StructuredIoctlArgType::Output).as_u32();
+
 /// Ioctl to get the EPID group ID
 pub const SGX_CMD_NUM_GET_EPID_GROUP_ID: u32 = StructuredIoctlNum::new::<sgx_epid_group_id_t>(
     1,
@@ -10,17 +11,20 @@ pub const SGX_CMD_NUM_GET_EPID_GROUP_ID: u32 = StructuredIoctlNum::new::<sgx_epi
     StructuredIoctlArgType::Output,
 )
 .as_u32();
-/// Ioctl to get quote
-pub const SGX_CMD_NUM_GEN_QUOTE: u32 = StructuredIoctlNum::new::<IoctlGenQuoteArg>(
+
+/// Ioctl to get EPID quote
+pub const SGX_CMD_NUM_GEN_EPID_QUOTE: u32 = StructuredIoctlNum::new::<IoctlGenEPIDQuoteArg>(
     2,
     SGX_MAGIC_CHAR,
     StructuredIoctlArgType::InputOutput,
 )
 .as_u32();
+
 /// Ioctl to get the target info of the current enclave
 pub const SGX_CMD_NUM_SELF_TARGET: u32 =
     StructuredIoctlNum::new::<sgx_target_info_t>(3, SGX_MAGIC_CHAR, StructuredIoctlArgType::Output)
         .as_u32();
+
 /// Ioctl to create a report
 pub const SGX_CMD_NUM_CREATE_REPORT: u32 = StructuredIoctlNum::new::<IoctlCreateReportArg>(
     4,
@@ -28,10 +32,43 @@ pub const SGX_CMD_NUM_CREATE_REPORT: u32 = StructuredIoctlNum::new::<IoctlCreate
     StructuredIoctlArgType::InputOutput,
 )
 .as_u32();
+
 /// Ioctl to verify a report
 pub const SGX_CMD_NUM_VERIFY_REPORT: u32 =
     StructuredIoctlNum::new::<sgx_report_t>(5, SGX_MAGIC_CHAR, StructuredIoctlArgType::Input)
         .as_u32();
+
+/// Ioctl to check if DCAP driver is installed on host
+pub const SGX_CMD_NUM_DETECT_DCAP_DRIVER: u32 =
+    StructuredIoctlNum::new::<i32>(6, SGX_MAGIC_CHAR, StructuredIoctlArgType::Output).as_u32();
+
+#[cfg(feature = "dcap")]
+/// Ioctl to get DCAP quote size
+pub const SGX_CMD_NUM_GET_DCAP_QUOTE_SIZE: u32 =
+    StructuredIoctlNum::new::<i32>(7, SGX_MAGIC_CHAR, StructuredIoctlArgType::Output).as_u32();
+
+#[cfg(feature = "dcap")]
+/// Ioctl to get DCAP quote
+pub const SGX_CMD_NUM_GEN_DCAP_QUOTE: u32 = StructuredIoctlNum::new::<IoctlGenDCAPQuoteArg>(
+    8,
+    SGX_MAGIC_CHAR,
+    StructuredIoctlArgType::InputOutput,
+)
+.as_u32();
+
+#[cfg(feature = "dcap")]
+/// Ioctl to get the verfication supplemental data size
+pub const SGX_CMD_NUM_GET_DCAP_SUPPLEMENTAL_SIZE: u32 =
+    StructuredIoctlNum::new::<i32>(9, SGX_MAGIC_CHAR, StructuredIoctlArgType::Output).as_u32();
+
+#[cfg(feature = "dcap")]
+/// Ioctl to verify DCAP quote
+pub const SGX_CMD_NUM_VER_DCAP_QUOTE: u32 = StructuredIoctlNum::new::<IoctlVerDCAPQuoteArg>(
+    10,
+    SGX_MAGIC_CHAR,
+    StructuredIoctlArgType::InputOutput,
+)
+.as_u32();
 
 /// A magical number that distinguishes SGX ioctls for other ioctls
 const SGX_MAGIC_CHAR: u8 = 's' as u8;
