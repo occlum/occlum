8c5966180a224ef20454e27fd13be73338757aac
zongmin.gu <zongmin.gzm@antgroup.com>
2021-02-07 21:05:35 +0800

    Update docker file to use offical SGX 2.13 PSW and DCAP binaries

--- tools/docker/start_aesm.sh
+++ tools/docker/start_aesm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Start AESM service required by Intel SGX SDK if it is not running
 if ! pgrep "aesm_service" > /dev/null ; then
-    LD_LIBRARY_PATH="/opt/intel/sgxpsw/aesm:$LD_LIBRARY_PATH" /opt/intel/sgxpsw/aesm/aesm_service
+    LD_LIBRARY_PATH="/opt/intel/sgx-aesm-service/aesm:$LD_LIBRARY_PATH" /opt/intel/sgx-aesm-service/aesm/aesm_service --no-daemon
 fi
