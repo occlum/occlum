d0331bd39747e7410e9413a00299b7584a529c2f
Hui, Chunyang <sanqian.hcy@antfin.com>
2021-03-11 05:57:43 +0000

    Add detection for FSGSBASE enablement in PAL

---
+++ src/pal/src/pal_check_fsgsbase.h
@@ -0,0 +1,8 @@
+#ifndef __PAL_CHECK_FSGSBASE_H__
+#define __PAL_CHECK_FSGSBASE_H__
+
+// Check if FSGSBASE instructions are supported.
+// Return 0 if the check pass.
+int check_fsgsbase_enablement(void);
+
+#endif
