8615b6177c9c744d719e3ef192325e69275a1ce9
Shirong Hao <shirong@linux.alibaba.com>
2021-05-18 13:10:53 +0000

    Update enclave-tls demo test

--- demos/enclave_tls/run_enclave_tls_server_in_occlum.sh
+++ demos/enclave_tls/run_enclave_tls_server_in_occlum.sh
@@ -8,12 +8,10 @@ cd occlum_workspace
 occlum init
 
 # 2. Copy files into Occlum Workspace and Build
-cp -f /opt/enclave-tls/bin/enclave-tls-server image/bin
+cp -f /usr/share/enclave-tls/samples/enclave-tls-server image/bin
 cp -f /opt/occlum/glibc/lib/libdl.so.2 image/opt/occlum/glibc/lib
 mkdir -p image/opt/enclave-tls
 cp -rf /opt/enclave-tls/lib image/opt/enclave-tls
-# The following libs are required by libenclave_quote_sgx_ecdsa.so
-cp /usr/lib/x86_64-linux-gnu/libsgx_dcap_quoteverify.so.1 image/opt/occlum/glibc/lib
 occlum build
 
 # 3. Run enclave_tls_server
