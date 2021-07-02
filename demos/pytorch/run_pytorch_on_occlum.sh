38e5966104623dd607223f40d15fd7af085bfaa5
LI Qing <geding.lq@antgroup.com>
2021-07-02 13:07:09 +0800

    test pytorch CI

--- demos/pytorch/run_pytorch_on_occlum.sh
+++ demos/pytorch/run_pytorch_on_occlum.sh
@@ -14,7 +14,7 @@ fi
 
 cd occlum_instance
 # Copy files into Occlum Workspace and build
-if [ ! -d "image/lib/python3" ];then
+if [ ! -L "image/bin/python3" ];then
     ln -s /opt/python-occlum/bin/python3 image/bin/python3
     cp -f /opt/occlum/glibc/lib/libdl.so.2 image/opt/occlum/glibc/lib/
     cp -f /opt/occlum/glibc/lib/libutil.so.1 image/opt/occlum/glibc/lib/
@@ -22,6 +22,7 @@ if [ ! -d "image/lib/python3" ];then
     cp -f ../demo.py image
     new_json="$(jq '.resource_limits.user_space_size = "6000MB" |
                     .resource_limits.kernel_space_heap_size = "256MB" |
+                    .resource_limits.max_num_of_threads = 64 |
                     .process.default_mmap_size = "4000MB" |
                     .env.default += ["PYTHONHOME=/opt/python-occlum"]' Occlum.json)" && \
     echo "${new_json}" > Occlum.json
