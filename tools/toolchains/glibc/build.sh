8011bb02d8ec327493d4d2916034f6b73d4e7257
sgui <sheng.gui@intel.com>
2021-07-05 15:19:35 +0800

    add Dockerfile for Ubuntu 20.04 add gcc check for glibc, cause gcc 9 will get some warning for old glibc version

--- tools/toolchains/glibc/build.sh
+++ tools/toolchains/glibc/build.sh
@@ -5,7 +5,12 @@ INSTALL_DIR=/opt/occlum/glibc
 
 # Exit if any command fails
 set -e
-
+GCCVERSION=`gcc -dumpversion | cut -f1 -d.`
+echo "GCCVERSION=$GCCVERSION"
+if [[ $GCCVERSION > 8 ]] ; then
+   EXTRA_FLAG=--disable-werror
+fi
+echo "EXTRA_FLAG=$EXTRA_FLAG"
 # Clean previous build and installation if any
 rm -rf ${SRC_DIR}
 rm -rf ${BUILD_DIR}
@@ -20,8 +25,11 @@ mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 # Build and install glibc
 unset LD_LIBRARY_PATH
+echo "CFLAGS="-O2 -g" ${SRC_DIR}/configure \
+  --prefix=${INSTALL_DIR} --with-tls --without-selinux \
+  --enable-stack-protector=strong --disable-nscd $EXTRA_FLAG"
 CFLAGS="-O2 -g" ${SRC_DIR}/configure \
   --prefix=${INSTALL_DIR} --with-tls --without-selinux \
-  --enable-stack-protector=strong --disable-nscd
+  --enable-stack-protector=strong --disable-nscd $EXTRA_FLAG
 make
 make install
