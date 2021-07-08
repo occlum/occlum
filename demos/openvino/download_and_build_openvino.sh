87f0ee0e14da41f2f0df70a811e65267f6931ad0
sgui <sheng.gui@intel.com>
2021-07-08 15:21:49 +0800

    Add support for OpenVINO 2021.3 1.Replace the original TBB with OneTBB v2020.3 2.Replace the 2019_R3 with DLDT 2021.3 3.Add some dependent lib to the occlum image

--- demos/openvino/download_and_build_openvino.sh
+++ demos/openvino/download_and_build_openvino.sh
@@ -28,36 +28,29 @@ build_opencv() {
       -DBUILD_opencv_python=OFF -DBUILD_PYTHON_SUPPORT=OFF \
       -DBUILD_EXAMPLES=OFF -DWITH_FFMPEG=OFF \
       -DWITH_QT=OFF -DWITH_CUDA=OFF
-    make -j4
+    make -j
     sudo make install
     popd
 }
 
+# Build oneTBB,  OpenVINO_2021 need version 2020.3
 build_tbb() {
     rm -rf deps/tbb_cmake && mkdir -p deps/tbb_cmake
     pushd deps/tbb_cmake
-    git clone https://github.com/wjakob/tbb .
-    git checkout 344fa84f34089681732a54f5def93a30a3056ab9
-    mkdir cmake_build && cd cmake_build
-    cmake ../ \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_CXX_COMPILER=occlum-g++ -DCMAKE_C_COMPILER=occlum-gcc \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DTBB_BUILD_TESTS=OFF \
-      -DTBB_BUILD_TBBMALLOC_PROXY=OFF
-    make
-    sudo make install
+    git clone https://github.com/oneapi-src/oneTBB.git .
+    git checkout v2020.3
+    CXX=occlum-g++ CC=occlum-gcc make tbb -j4
+    find build/ -name libtbb* -exec cp {} $PREFIX/lib/ \;
     popd
 }
+
 
 # Build OpenVINO
 build_openvino() {
     rm -rf openvino_src && mkdir openvino_src
     pushd openvino_src
     git clone https://github.com/opencv/dldt .
-    git checkout tags/2019_R3 -b 2019_R3
-    git apply ../0001-Fix-passing-pre-increment-parameter-cpu-to-CPU_ISSET.patch
-    cd inference-engine
+    git checkout 2021.3
     git submodule init
     git submodule update --recursive
     mkdir build && cd build
@@ -68,8 +61,8 @@ build_openvino() {
       -DENABLE_CLDNN=OFF \
       -DENABLE_MYRIAD=OFF \
       -DENABLE_GNA=OFF
-    [ "$THREADING" == "OMP" ] && rm -rf ../temp/omp/lib/* && cp $PREFIX/lib/libgomp.so ../temp/omp/lib/libiomp5.so
-    [ "$THREADING" == "TBB" ] && rm -rf ../temp/tbb/lib/* && cp $PREFIX/lib/libtbb.so ../temp/tbb/lib && cp $PREFIX/lib/libtbbmalloc.so ../temp/tbb/lib
+    [ "$THREADING" == "OMP" ] && rm -rf ../inference-engine/temp/omp/lib/* && cp $PREFIX/lib/libgomp.so ../inference-engine/temp/omp/lib/libiomp5.so
+    [ "$THREADING" == "TBB" ] && rm -rf ../inference-engine/temp/tbb/lib/* && cp $PREFIX/lib/libtbb.so.2 ../inference-engine/temp/tbb/lib
     cd ../
     rm -rf build && mkdir build && cd build
 #   Substitute OpenCV
@@ -84,7 +77,8 @@ build_openvino() {
       -DENABLE_CLDNN=OFF \
       -DENABLE_OPENCV=OFF \
       -DENABLE_MYRIAD=OFF \
-      -DENABLE_GNA=OFF
+      -DENABLE_GNA=OFF \
+      -DENABLE_VPU=OFF
     make -j4
     popd
 }
