e59a82180bc97540004a68d5e0438f5463866f1f
Hui, Chunyang <sanqian.hcy@antfin.com>
2021-07-09 10:05:13 +0000

    Re-enable aliyunlinux 3 image as default aliyunlinux image

--- tools/docker/build_image.sh
+++ tools/docker/build_image.sh
@@ -21,6 +21,7 @@ USAGE:
     The name of the OS distribution that the Docker image is based on. Currently, <OS_NAME> must be one of the following values:
         ubuntu18.04         Use Ubuntu 18.04 as the base image
         centos8.2           Use CentOS 8.2 as the base image
+        aliyunlinux3        Use AliyunLinux 3 as the base image
 
 The resulting Docker image will have "occlum/occlum:<OCCLUM_LABEL>-<OS_NAME>" as its label.
 EOF
@@ -43,7 +44,7 @@ function check_item_in_list() {
     [[ $list =~ (^|[[:space:]])$item($|[[:space:]]) ]]
 }
 
-check_item_in_list "$os_name" "ubuntu18.04 centos8.2" || report_error
+check_item_in_list "$os_name" "ubuntu18.04 centos8.2 aliyunlinux3" || report_error
 
 cd "$script_dir/.."
 docker build -f "$script_dir/Dockerfile.$os_name" -t "occlum/occlum:$occlum_label-$os_name" --build-arg OCCLUM_BRANCH=$occlum_branch .
