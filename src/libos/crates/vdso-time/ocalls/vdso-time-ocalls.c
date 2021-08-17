b52848c30c38811a25bc83946bb0169455a1cff6
Shuocheng Wang <wangshch5@outlook.com>
2021-08-17 20:03:40 +0800

    [pal] Fix compiling error of external ocalls

--- src/libos/crates/vdso-time/ocalls/vdso-time-ocalls.c
+++ src/libos/crates/vdso-time/ocalls/vdso-time-ocalls.c
@@ -4,34 +4,34 @@
 #include <string.h>
 
 int vdso_ocall_get_vdso_info(
-    unsigned long* vdso_addr, 
-    long* hres_resolution,
-    long* coarse_resolution,
-    char* release,
+    unsigned long *vdso_addr,
+    long *hres_resolution,
+    long *coarse_resolution,
+    char *release,
     int release_len,
-    struct timespec* tss,
-    int tss_len) 
-{
+    struct timespec *tss,
+    int tss_len) {
     *vdso_addr = getauxval(AT_SYSINFO_EHDR);
 
     struct timespec tp;
     if (!clock_getres(CLOCK_REALTIME, &tp)) {
         *hres_resolution = tp.tv_nsec;
-    } else *hres_resolution = 0;
+    } else { *hres_resolution = 0; }
     if (!clock_getres(CLOCK_REALTIME_COARSE, &tp)) {
         *coarse_resolution = tp.tv_nsec;
-    } else *coarse_resolution = 0;
+    } else { *coarse_resolution = 0; }
 
     struct utsname buf;
     int ret = uname(&buf);
-    if (ret != 0) return -1;
+    if (ret != 0) { return -1; }
 
     int buf_rel_len = strlen(buf.release);
     int len = buf_rel_len > release_len ? release_len : buf_rel_len;
     memcpy(release, buf.release, len);
 
-    clockid_t clockids[] = { CLOCK_REALTIME, CLOCK_MONOTONIC, CLOCK_MONOTONIC_RAW, 
-        CLOCK_REALTIME_COARSE, CLOCK_MONOTONIC_COARSE, CLOCK_BOOTTIME };
+    clockid_t clockids[] = { CLOCK_REALTIME, CLOCK_MONOTONIC, CLOCK_MONOTONIC_RAW,
+                             CLOCK_REALTIME_COARSE, CLOCK_MONOTONIC_COARSE, CLOCK_BOOTTIME
+                           };
     for (int i = 0; i < sizeof(clockids) / sizeof(clockid_t); ++i) {
         if (tss_len > clockids[i] && clock_gettime(clockids[i], &tss[clockids[i]]) != 0) {
             tss[clockids[i]].tv_sec = 0;
