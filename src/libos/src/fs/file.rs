occlum/libos

Add support for the mode of fallocate

src/libos/src/fs/file.rs
@@ -91,7 +91,7 @@ pub trait File: Debug + Sync + Send + Any {
         return_op_unsupported_error!("set_advisory_lock")
     }
 
-    fn fallocate(&self, _mode: u32, _offset: u64, _len: u64) -> Result<()> {
+    fn fallocate(&self, _mode: FallocateMode, _offset: usize, _len: usize) -> Result<()> {
         return_op_unsupported_error!("fallocate")
     }
 

src/libos/src/fs/syscalls.rs
@@ -545,12 +545,8 @@ pub fn do_fallocate(fd: FileDesc, mode: u32, offset: off_t, len: off_t) -> Resul
             "offset was less than 0, or len was less than or equal to 0"
         );
     }
-    // Current implementation is just the posix_fallocate
-    // TODO: Support more modes in fallocate
-    if mode != 0 {
-        return_errno!(ENOSYS, "unsupported mode");
-    }
-    file_ops::do_fallocate(fd, mode, offset as u64, len as u64)?;
+    let mode = FallocateMode::from_u32(mode)?;
+    file_ops::do_fallocate(fd, mode, offset as usize, len as usize)?;
     Ok(0)
 }
 

occlum/occlum

src/libos/src/fs/file.rs
@@ -91,7 +91,7 @@ pub trait File: Debug + Sync + Send + Any {
         return_op_unsupported_error!("set_advisory_lock")
     }
 
-    fn fallocate(&self, _mode: u32, _offset: u64, _len: u64) -> Result<()> {
+    fn fallocate(&self, _flags: FallocateFlags, _offset: usize, _len: usize) -> Result<()> {
         return_op_unsupported_error!("fallocate")
     }
 

src/libos/src/fs/syscalls.rs
@@ -628,12 +628,8 @@ pub fn do_fallocate(fd: FileDesc, mode: u32, offset: off_t, len: off_t) -> Resul
             "offset was less than 0, or len was less than or equal to 0"
         );
     }
-    // Current implementation is just the posix_fallocate
-    // TODO: Support more modes in fallocate
-    if mode != 0 {
-        return_errno!(ENOSYS, "unsupported mode");
-    }
-    file_ops::do_fallocate(fd, mode, offset as u64, len as u64)?;
+    let flags = FallocateFlags::from_u32(mode)?;
+    file_ops::do_fallocate(fd, flags, offset as usize, len as usize)?;
     Ok(0)
 }
 