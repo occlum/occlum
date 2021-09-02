a209034dfb25b1c53a45e5aeb198b35143ec1363
Shuocheng Wang <wangshch5@outlook.com>
2021-09-02 19:06:23 +0800

    [async-rt] Add timeout tests and update doc

--- src/libos/crates/async-rt/README.md
+++ src/libos/crates/async-rt/README.md
@@ -11,5 +11,5 @@ async-rt = { path = "your_path/async-rt" }
 
 if use async-rt in SGX (based on rust-sgx-sdk), place the following line under the `[dependencies]` section in your `Cargo.toml` and prepare incubator-teaclave-sgx-sdk envirenments according to async-rt's `Cargo.toml`:
 ```
-async-rt = { path = "your_path/async-rt", features = ["sgx"]  }
+async-rt = { path = "your_path/async-rt", default-features = false, features = ["sgx"]  }
 ```
--- src/libos/crates/async-rt/src/wait/mod.rs
+++ src/libos/crates/async-rt/src/wait/mod.rs
@@ -8,10 +8,9 @@ pub use self::waiter_queue::WaiterQueue;
 
 #[cfg(test)]
 mod tests {
-    use std::sync::Arc;
-
     use self::queue::Queue;
     use super::*;
+    use crate::prelude::*;
 
     mod queue {
         use super::*;
@@ -34,30 +33,30 @@ mod tests {
                 }
             }
 
-            pub async fn produce(&self, item: T) {
-                crate::waiter_loop!(&self.producers, {
+            pub async fn produce(&self, item: T, mut timeout: Option<&mut Duration>) -> Result<()> {
+                crate::waiter_loop!(&self.producers, timeout, {
                     let mut buf = self.buf.lock().unwrap();
                     if buf.len() < buf.capacity() {
                         buf.push_back(item);
                         drop(buf);
 
                         self.consumers.wake_one();
-                        return;
+                        return Ok(());
                     }
-                });
+                })
             }
 
-            pub async fn consume(&self) -> T {
-                crate::waiter_loop!(&self.consumers, {
+            pub async fn consume(&self, mut timeout: Option<&mut Duration>) -> Result<T> {
+                crate::waiter_loop!(&self.consumers, timeout, {
                     let mut buf = self.buf.lock().unwrap();
                     if buf.len() > 0 {
                         let item = buf.pop_front().unwrap();
                         drop(buf);
 
                         self.producers.wake_one();
-                        return item;
+                        return Ok(item);
                     }
-                });
+                })
             }
 
             pub fn len(&self) -> usize {
@@ -82,8 +81,9 @@ mod tests {
             let producer_task = {
                 let queue = queue.clone();
                 crate::task::spawn(async move {
+                    let mut timeout = Some(Duration::from_millis(500));
                     for i in 0..num_items {
-                        queue.produce(i).await;
+                        queue.produce(i, timeout.as_mut()).await.unwrap();
                     }
                 })
             };
@@ -91,7 +91,7 @@ mod tests {
                 let queue = queue.clone();
                 crate::task::spawn(async move {
                     for i in 0..num_items {
-                        assert!(queue.consume().await == i);
+                        assert!(queue.consume(None).await.unwrap() == i);
                     }
                 })
             };
@@ -99,5 +99,89 @@ mod tests {
             producer_task.await;
             consumer_task.await;
         });
+    }
+
+    #[test]
+    fn wait_timeout_err() {
+        crate::task::block_on(async {
+            let ms = 100;
+            let mut timeout = Duration::from_millis(ms);
+            let start = std::time::Instant::now();
+            imagined_blocking_func1(Some(&mut timeout)).await;
+            assert!(timeout.is_zero());
+            assert!(start.elapsed().as_millis() as u64 >= ms - 1);
+
+            let mut timeout = Duration::from_millis(ms);
+            let start = std::time::Instant::now();
+            imagined_blocking_func2(Some(&mut timeout)).await;
+            assert!(timeout.is_zero());
+            assert!(start.elapsed().as_millis() as u64 >= ms - 1);
+
+            // case: timeout less than 1ms.
+            let mut timeout = Duration::from_nanos(10);
+            let start = std::time::Instant::now();
+            imagined_blocking_func1(Some(&mut timeout)).await;
+            assert!(timeout.is_zero());
+            assert!(start.elapsed().as_millis() < 2);
+        });
+    }
+
+    #[test]
+    fn wait_timeout_ok() {
+        crate::task::block_on(async {
+            let waiter = Waiter::new();
+            let waker = waiter.waker();
+
+            let sleep_time = 10;
+            let timeout_time = 100;
+            let join_handle = crate::task::spawn(async move {
+                let mut timeout = Duration::from_millis(timeout_time);
+                assert!(waiter.wait_timeout(Some(&mut timeout)).await.is_ok());
+            });
+
+            crate::sched::yield_().await;
+            std::thread::sleep(Duration::from_millis(sleep_time));
+            waker.wake();
+            join_handle.await;
+        });
+    }
+
+    #[test]
+    fn wait_none() {
+        crate::task::block_on(async {
+            let waiter = Waiter::new();
+            let waker = waiter.waker();
+
+            let join_handle = crate::task::spawn(async move {
+                waiter.wait_timeout::<Duration>(None).await.unwrap();
+            });
+
+            crate::sched::yield_().await;
+            waker.wake();
+            join_handle.await;
+        });
+    }
+
+    async fn imagined_blocking_func1(timeout: Option<&mut Duration>) {
+        assert!(timeout.is_some());
+
+        let waiter = Waiter::new();
+        let res = waiter.wait_timeout(timeout).await;
+
+        // timeout expired.
+        assert!(res.is_err());
+    }
+
+    async fn imagined_blocking_func2(timeout: Option<&mut Duration>) {
+        assert!(timeout.is_some());
+        let mut timeout = timeout.map_or(None, |t| Some(t));
+
+        let waiter = Waiter::new();
+        loop {
+            if let Err(_) = waiter.wait_timeout(timeout.as_mut()).await {
+                // timeout expired, return.
+                break;
+            }
+        }
     }
 }
