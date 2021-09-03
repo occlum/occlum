e85c87be70047b2bf6a3ffc50151a62b4c418bbc
LI Qing <geding.lq@antgroup.com>
2021-09-03 10:51:10 +0800

    Modify the statfs on SEFS or UnionFS with ocall

--- src/pal/include/edl/occlum_edl_types.h
+++ src/pal/include/edl/occlum_edl_types.h
@@ -5,6 +5,7 @@
 #include <sys/select.h>         // import fd_set
 #include <sys/time.h>           // import struct timeval
 #include <sys/uio.h>            // import struct iovec
+#include <sys/vfs.h>            // import struct statfs
 #include <occlum_pal_api.h>     // import occlum_stdio_fds
 
 #endif /* __OCCLUM_EDL_TYPES__ */
