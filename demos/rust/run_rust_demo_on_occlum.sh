ea1a56dbfa8ac99050e9695ef37b56f649219f8d
jiangjianfeng <jiangjianfeng.jjf@antgroup.com>
2021-09-17 14:47:36 +0800

    Modify rust demo with copy_bom

--- demos/rust/run_rust_demo_on_occlum.sh
+++ demos/rust/run_rust_demo_on_occlum.sh
@@ -9,8 +9,8 @@ popd
 # initialize occlum workspace
 rm -rf occlum_instance && mkdir occlum_instance && cd occlum_instance
 
-occlum init
-cp ../rust_app/target/x86_64-unknown-linux-musl/debug/rust_app image/bin
+occlum init && rm -rf image
+copy_bom -f ../rust-demo.yaml --root image --include-dir /opt/occlum/etc/template
 
 occlum build
 occlum run /bin/rust_app
