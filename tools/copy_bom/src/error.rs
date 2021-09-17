6a0435d9b6bedaf30292d1fa4ff53fe1ab813822
jiangjianfeng <jiangjianfeng.jjf@antgroup.com>
2021-09-17 11:57:07 +0800

    Define error numbers

---
+++ tools/copy_bom/src/error.rs
@@ -0,0 +1,8 @@
+/// This file defines error number to indicate different errors
+pub static FILE_NOT_EXISTS_ERROR: i32 = -1;
+pub static COPY_FILE_ERROR: i32 = -2;
+pub static INVALID_BOM_FILE_ERROR: i32 = -3;
+pub static CREATE_DIR_ERROR: i32 = -4;
+pub static CREATE_SYMLINK_ERROR: i32 = -5;
+pub static COPY_DIR_ERROR: i32 = -6;
+pub static INCORRECT_HASH_ERROR: i32 = -7;
