47a8de0dcf59d066812b597d60c04e0c64047c1d
Zheng, Qi <huaiqing.zq@antgroup.com>
2021-09-22 14:25:52 +0800

    Use toolchains from the git clone source instead of from the local path
    Signed-off-by: Zheng, Qi <huaiqing.zq@antgroup.com>

--- tools/docker/build_image.sh
+++ tools/docker/build_image.sh
@@ -12,7 +12,7 @@ build_image
 Build an Occlum Docker image for a specific OS
 
 USAGE:
-    build_image.sh <OCCLUM_LABEL> <OS_NAME>
+    build_image.sh <OCCLUM_LABEL> <OS_NAME> <OCCLUM_BRANCH>
 
 <OCCLUM_LABEL>:
     An arbitrary string chosen by the user to describe the version of Occlum preinstalled in the Docker image, e.g., "latest", "0.12.0", "prerelease", and etc.
@@ -22,6 +22,10 @@ USAGE:
         ubuntu18.04         Use Ubuntu 18.04 as the base image
         centos8.2           Use CentOS 8.2 as the base image
         aliyunlinux3        Use AliyunLinux 3 as the base image
+
+<OCCLUM_BRANCH>:
+    The Occlum branch which the docker image is built on, e.g "0.24.0".
+    It is optional, if not provided, "master" branch will be used.
 
 The resulting Docker image will have "occlum/occlum:<OCCLUM_LABEL>-<OS_NAME>" as its label.
 EOF
@@ -34,9 +38,9 @@ if [[ ( "$#" < 2 ) ]] ; then
     report_error
 fi
 
-occlum_branch=master
 occlum_label=$1
 os_name=$2
+occlum_branch=${3:-master}
 
 function check_item_in_list() {
     item=$1
