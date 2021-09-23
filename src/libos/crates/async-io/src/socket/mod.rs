43b1ffe042c8a49dc2ee17091bcb353840512f6f
Shuocheng Wang <wangshch5@outlook.com>
2021-09-23 14:56:02 +0800

    [async-io] Add socket type

--- src/libos/crates/async-io/src/socket/mod.rs
+++ src/libos/crates/async-io/src/socket/mod.rs
@@ -1,7 +1,9 @@
 mod addr;
 mod domain;
 mod shutdown;
+mod r#type;
 
 pub use self::addr::{Addr, CSockAddr, Ipv4Addr, Ipv4SocketAddr, UnixAddr};
 pub use self::domain::Domain;
+pub use self::r#type::Type;
 pub use self::shutdown::Shutdown;
