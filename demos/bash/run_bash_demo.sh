597f8c908066a17b62af7090a2676689bc5d15ff
Zheng, Qi <huaiqing.zq@antgroup.com>
2021-09-24 15:45:58 +0800

    Update bash demo with bom tool and add musl version
    Signed-off-by: Zheng, Qi <huaiqing.zq@antgroup.com>

--- demos/bash/run_bash_demo.sh
+++ demos/bash/run_bash_demo.sh
@@ -1,29 +1,27 @@
 #! /bin/bash
 set -e
 
-current=$(pwd)
-bash_src="$current/bash_for_occlum"
-busybox_src="$current/busybox"
-occlum_instance="$current/occlum_instance"
-occlum_glibc_path=$occlum_instance/image/opt/occlum/glibc/lib
-# Executable path in Occlum
-exec_path=/root/bin
+if [[ $1 == "musl" ]]; then
+    echo "*** Run musl-libc bash demo ***"
+    ./prepare_busybox.sh musl
+    bomfile="../bash-musl.yaml"
+else
+    echo "*** Run glibc bash demo ***"
+    ./prepare_busybox.sh
+    bomfile="../bash.yaml"
+fi
 
 rm -rf occlum_instance
 occlum new occlum_instance
 
-cd occlum_instance
+pushd occlum_instance
+rm -rf image
+copy_bom -f $bomfile --root image --include-dir /opt/occlum/etc/template
 
-cp $bash_src/bash ./image/bin/
-cp /lib/x86_64-linux-gnu/libtinfo.so.5 $occlum_glibc_path
-cp /lib/x86_64-linux-gnu/libdl.so.2 $occlum_glibc_path
-cp $busybox_src/busybox image/bin
-cp /lib/x86_64-linux-gnu/libm.so.6 $occlum_glibc_path
-cp /lib/x86_64-linux-gnu/libresolv.so.2 $occlum_glibc_path
-
-mkdir -p "$occlum_instance/image/$exec_path"
-cp "$current/occlum_bash_test.sh" "$occlum_instance/image/$exec_path"
-cp "$current/Occlum.json" "$occlum_instance"
+new_json="$(jq '.resource_limits.user_space_size = "600MB" ' Occlum.json)" && \
+    echo "${new_json}" > Occlum.json
 
 occlum build
-occlum run /root/bin/occlum_bash_test.sh
+occlum run /bin/occlum_bash_test.sh
+
+popd
