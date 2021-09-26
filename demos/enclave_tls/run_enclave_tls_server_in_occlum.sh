4a91c11f9f3b2ac391a53f766b385322c8a2d6fc
zongmin.gu <zongmin.gzm@antgroup.com>
2021-09-26 13:47:28 +0800

    Update enclave_tls demo with bom file

--- demos/enclave_tls/run_enclave_tls_server_in_occlum.sh
+++ demos/enclave_tls/run_enclave_tls_server_in_occlum.sh
@@ -8,12 +8,8 @@ cd occlum_workspace
 occlum init
 
 # 2. Copy files into Occlum Workspace and Build
-cp -f /usr/share/enclave-tls/samples/enclave-tls-server image/bin
-cp -f /opt/occlum/glibc/lib/libdl.so.2 image/opt/occlum/glibc/lib
-cp -f /usr/lib/x86_64-linux-gnu/libssl.so.1.1 image/opt/occlum/glibc/lib
-cp -f /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1 image/opt/occlum/glibc/lib
-mkdir -p image/opt/enclave-tls
-cp -rf /opt/enclave-tls/lib image/opt/enclave-tls
+rm -rf image && \
+copy_bom -f ../enclave_tls.yaml --root image --include-dir /opt/occlum/etc/template && \
 occlum build
 
 # 3. Run enclave_tls_server
