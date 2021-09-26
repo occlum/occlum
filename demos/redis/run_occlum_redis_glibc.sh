04f1e5577dab8d58f188ec80317a31f6496b5e22
Zheng, Qi <huaiqing.zq@antgroup.com>
2021-09-26 11:04:59 +0800

    Update redis demo with copy_bom tool
    Signed-off-by: Zheng, Qi <huaiqing.zq@antgroup.com>

--- demos/redis/run_occlum_redis_glibc.sh
+++ demos/redis/run_occlum_redis_glibc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-redis_dir=/usr/local/redis/
-occlum_glibc=/opt/occlum/glibc/lib/
 set -e
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+bomfile=${SCRIPT_DIR}/redis_glibc.yaml
 
 # 1. Init Occlum Workspace
 rm -rf occlum_instance
@@ -11,14 +12,9 @@ new_json="$(jq '.resource_limits.user_space_size = "320MB" |
                 .process.default_mmap_size = "256MB"' Occlum.json)" && \
 echo "${new_json}" > Occlum.json
 
-# 2. Copy files into Occlum Workspace and Build
-cp $redis_dir/bin/redis* image/bin
-cp /usr/local/bin/openssl* image/bin
-cp /usr/local/lib/libssl* image/$occlum_glibc
-cp /usr/local/lib/libcrypto* image/$occlum_glibc
-cp $occlum_glibc/libdl.so.2 image/$occlum_glibc
-cp $occlum_glibc/librt.so.1 image/$occlum_glibc
-cp $occlum_glibc/libm.so.6 image/$occlum_glibc
+rm -rf image
+copy_bom -f $bomfile --root image --include-dir /opt/occlum/etc/template
+
 #occlum build
 occlum build
 # 3. Run redis server
