09b79083b23921167168c8ccf2b22019dfbc33b8
Zheng, Qi <huaiqing.zq@antgroup.com>
2021-09-26 11:04:59 +0800

    Update redis demo with copy_bom tool
    Signed-off-by: Zheng, Qi <huaiqing.zq@antgroup.com>

--- demos/redis/benchmark.sh
+++ demos/redis/benchmark.sh
@@ -1,4 +1,4 @@
 ./run_occlum_redis.sh &
 sleep 20
 echo 'start client'
-/usr/local/occlum/x86_64-linux-musl/bin/redis-benchmark -n 1000
+/usr/local/occlum/x86_64-linux-musl/redis/bin/redis-benchmark -n 1000
