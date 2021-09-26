16384b8b0e249cc543ad75447e5503874c14dd3f
jianfengjiang <jiangjianfeng.jjf@antgroup.com>
2021-09-26 17:06:18 +0800

    Update python-musl demo with copy_bom

--- demos/python/python_musl/run_python_on_occlum.sh
+++ demos/python/python_musl/run_python_on_occlum.sh
@@ -18,22 +18,8 @@ cd occlum_instance
 
 # 2. Copy files into Occlum Workspace and build
 if [ ! -d "image/lib/python3.7" ];then
-    cp -f $alpine_fs/usr/local/bin/python3.7 image/bin
-    cp -f $alpine_fs/usr/local/lib/libpython3.7m.so.1.0 image/lib
-    cp -f $alpine_fs/usr/local/lib/libpython3.so image/lib
-    cp -rf $alpine_fs/usr/local/lib/python3.7 image/lib
-    cp -f $alpine_fs/usr/lib/libblas.so.3 image/lib
-    cp -f $alpine_fs/usr/lib/libcblas.so.3 image/lib
-    cp -f $alpine_fs/usr/lib/libbz2.so.1 image/lib
-    cp -f $alpine_fs/usr/lib/libffi.so.6 image/lib
-    cp -f $alpine_fs/usr/lib/libgcc_s.so.1 image/lib
-    cp -f $alpine_fs/usr/lib/libgfortran.so.5 image/lib
-    cp -f $alpine_fs/usr/lib/liblapack.so.3 image/lib
-    cp -f $alpine_fs/usr/lib/liblzma.so.5 image/lib
-    cp -f $alpine_fs/usr/lib/libquadmath.so.0 image/lib
-    cp -f $alpine_fs/lib/libz.so.1 image/lib
-    cp -rf ../dataset image
-    cp -f ../demo.py image
+    rm -rf image
+    copy_bom -f ../python_musl.yaml --root image --include-dir /opt/occlum/etc/template
     new_json="$(jq '.resource_limits.user_space_size = "320MB" |
                     .resource_limits.kernel_space_heap_size = "256MB" |
                     .process.default_mmap_size = "256MB"' Occlum.json)" && \
