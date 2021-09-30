245ecdb5b7a283d25d62a750c42aea62d8c5c11d
Shuocheng Wang <wangshch5@outlook.com>
2021-10-01 02:32:11 +0800

    [async-io] Add socket send / recv flags

---
+++ src/libos/crates/async-io/src/socket/flags.rs
@@ -0,0 +1,38 @@
+use bitflags::bitflags;
+
+// Flags to use when sending data through a socket
+bitflags! {
+    pub struct SendFlags: i32 {
+        const MSG_OOB          = 0x01;      // Sends out-of-band data on sockets
+        const MSG_DONTROUTE    = 0x04;      // Don't use a gateway to send out the packet
+        const MSG_DONTWAIT     = 0x40;      // Nonblocking io
+        const MSG_EOR          = 0x80;      // End of record
+        const MSG_CONFIRM      = 0x0800;    // Confirm path validity
+        const MSG_NOSIGNAL     = 0x4000;    // Do not generate SIGPIPE
+        const MSG_MORE         = 0x8000;    // Sender will send more
+    }
+}
+
+// Flags to use when receiving data through a socket
+bitflags! {
+    pub struct RecvFlags: i32 {
+        const MSG_OOB          = 0x01;          // Recv out-of-band data
+        const MSG_PEEK         = 0x02;          // Return data without removing that
+        const MSG_TRUNC        = 0x20;          // Return the real length of the packet or datagram
+        const MSG_DONTWAIT     = 0x40;          // Nonblocking io
+        const MSG_WAITALL      = 0x0100;        // Wait for a full request
+        const MSG_ERRQUEUE     = 0x2000;        // Fetch message from error queue
+        // recvmsg only
+        const MSG_CMSG_CLOEXEC = 0x40000000;    // Set close_on_exec for file descriptor received through SCM_RIGHTS
+    }
+}
+
+bitflags! {
+    pub struct MsgFlags: i32 {
+        const MSG_OOB          = 0x01;      // Expedited or out-of-band data was received
+        const MSG_CTRUNC       = 0x08;      // Some control data was discarded
+        const MSG_TRUNC        = 0x20;      // The trailing portion of a datagram was discarded
+        const MSG_EOR          = 0x80;      // End of record
+        const MSG_ERRQUEUE     = 0x2000;    // Fetch message from error queue
+    }
+}
--- src/libos/crates/async-io/src/socket/mod.rs
+++ src/libos/crates/async-io/src/socket/mod.rs
@@ -1,9 +1,11 @@
 mod addr;
 mod domain;
+mod flags;
 mod shutdown;
 mod r#type;
 
 pub use self::addr::{Addr, CSockAddr, Ipv4Addr, Ipv4SocketAddr, UnixAddr};
 pub use self::domain::Domain;
+pub use self::flags::{MsgFlags, RecvFlags, SendFlags};
 pub use self::r#type::Type;
 pub use self::shutdown::Shutdown;
