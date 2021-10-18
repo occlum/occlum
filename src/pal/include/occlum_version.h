59986df1a57007fc7770b3846f25c73b178d6042
zongmin.gu <zongmin.gzm@antgroup.com>
2021-10-18 14:02:12 +0800

    Bump version to 0.24.2

--- src/pal/include/occlum_version.h
+++ src/pal/include/occlum_version.h
@@ -3,10 +3,10 @@
 #ifndef _OCCLUM_VERSION_H_
 #define _OCCLUM_VERSION_H_
 
-// Version = 0.24.1
+// Version = 0.24.2
 #define OCCLUM_MAJOR_VERSION    0
 #define OCCLUM_MINOR_VERSION    24
-#define OCCLUM_PATCH_VERSION    1
+#define OCCLUM_PATCH_VERSION    2
 
 #define STRINGIZE_PRE(X) #X
 #define STRINGIZE(X) STRINGIZE_PRE(X)
