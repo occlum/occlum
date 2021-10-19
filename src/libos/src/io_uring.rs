647dbd78908f855fa9f186e31f75d27fb2020823
Shuocheng Wang <shuocheng.wsc@antgroup.com>
2021-10-19 15:14:13 +0800

    [libos] Use larger queue size for io-uring

--- src/libos/src/io_uring.rs
+++ src/libos/src/io_uring.rs
@@ -2,7 +2,7 @@ use io_uring_callback::{Builder, IoUring};
 
 lazy_static::lazy_static! {
     pub static ref SINGLETON: IoUring = {
-        let io_uring = Builder::new().build(128).unwrap();
+        let io_uring = Builder::new().build(256).unwrap();
         unsafe {
             io_uring.start_enter_syscall_thread();
         }
