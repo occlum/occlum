8b27fdcea975ef5d5c5a56cd062cf967b759cc04
Zheng, Qi <huaiqing.zq@antgroup.com>
2021-10-19 10:18:25 +0800

    Add linux ltp demo
    Signed-off-by: Zheng, Qi <huaiqing.zq@antgroup.com>

---
+++ demos/linux-ltp/dl_and_build_ltp.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+set -e
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+TAG=20210927
+LTP_INSTALL_DIR=${SCRIPT_DIR}/ltp_install/ltp
+
+rm -rf ltp
+rm -rf $LTP_INSTALL_DIR && mkdir -p ${LTP_INSTALL_DIR}
+git clone -b $TAG https://github.com/linux-test-project/ltp.git
+
+pushd ltp
+# Apply patch to support running ltp in Occlum
+git apply ../0001-Make-it-work-on-Occlum.patch
+git apply ../0002-Ignore-some-test-cases-which-crash-the-Occlum.patch
+make autotools
+./configure --prefix=${LTP_INSTALL_DIR}
+make -j && make install
+
+popd
---
+++ demos/linux-ltp/prepare_ltp.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+set -e
+
+rm -rf ltp_instance
+occlum new ltp_instance
+
+cd ltp_instance
+rm -rf image
+copy_bom -f ../ltp.yaml --root image --include-dir /opt/occlum/etc/template
+
+new_json="$(jq '.resource_limits.user_space_size = "3000MB" |
+                .resource_limits.kernel_space_heap_size ="512MB" |
+                .resource_limits.kernel_space_stack_size ="4MB" |
+                .resource_limits.max_num_of_threads = 96 |
+                .entry_points = [ "/opt/ltp" ] |
+                .env.default = [ "OCCLUM=yes", "LTPROOT=/opt/ltp", "TMP=/tmp", "HOME=/root" ]' Occlum.json)" && \
+echo "${new_json}" > Occlum.json
+
+occlum build
+
