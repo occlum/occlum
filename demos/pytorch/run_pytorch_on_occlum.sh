e2d2cb375c51ddc5a1952742b3bb0b6b62ad6c78
Zheng, Qi <huaiqing.zq@antgroup.com>
2021-10-21 19:53:38 +0800

    Increase the pytorch demo TCS number to 64
    Also increase ready check delay time to 360s
    
    Signed-off-by: Zheng, Qi <huaiqing.zq@antgroup.com>

--- demos/pytorch/run_pytorch_on_occlum.sh
+++ demos/pytorch/run_pytorch_on_occlum.sh
@@ -17,7 +17,7 @@ fi
 
 new_json="$(jq '.resource_limits.user_space_size = "6000MB" |
                 .resource_limits.kernel_space_heap_size = "256MB" |
-                .process.default_mmap_size = "4000MB" |
+                .resource_limits.max_num_of_threads = 64 |
                 .env.default += ["PYTHONHOME=/opt/python-occlum"]' Occlum.json)" && \
 echo "${new_json}" > Occlum.json
 occlum build
