3b961fece83edd2d05a685a792e1c28c6ba55740
Shuocheng Wang <shuocheng.wsc@antgroup.com>
2021-10-25 11:56:42 +0800

    [pal] Remove useless sched ocalls

--- src/pal/src/ocalls/sched.c
+++ src/pal/src/ocalls/sched.c
@@ -4,30 +4,6 @@
 #include <unistd.h>
 #include "ocalls.h"
 
-int occlum_ocall_sched_getaffinity(size_t cpusize, unsigned char *buf) {
-    int ret;
-    cpu_set_t mask;
-    CPU_ZERO(&mask);
-
-    ret = syscall(__NR_sched_getaffinity, GETTID(), sizeof(cpu_set_t), &mask);
-    memcpy(buf, &mask, cpusize);
-    return ret;
-}
-
-int occlum_ocall_sched_setaffinity(int host_tid, size_t cpusize,
-                                   const unsigned char *buf) {
-    return syscall(__NR_sched_setaffinity, host_tid, cpusize, buf);
-}
-
-/* In the Linux implementation, sched_yield() always succeeds */
-void occlum_ocall_sched_yield(void) {
-    sched_yield();
-}
-
-int occlum_ocall_ncores(void) {
-    return sysconf(_SC_NPROCESSORS_CONF);
-}
-
 static int is_number(const char *str) {
     size_t len = strlen(str);
     for (size_t i = 0; i < len; i++) {
