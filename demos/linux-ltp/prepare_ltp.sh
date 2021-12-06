e8458bd27c7e5d9c9dbc851762dfd9c7a92c1a33
Zheng, Qi <huaiqing.zq@antgroup.com>
2021-12-06 19:43:56 +0800

    Enlarge kernel heap size for ltp demo

--- demos/linux-ltp/prepare_ltp.sh
+++ demos/linux-ltp/prepare_ltp.sh
@@ -9,7 +9,7 @@ rm -rf image
 copy_bom -f ../ltp.yaml --root image --include-dir /opt/occlum/etc/template
 
 new_json="$(jq '.resource_limits.user_space_size = "3000MB" |
-                .resource_limits.kernel_space_heap_size ="512MB" |
+                .resource_limits.kernel_space_heap_size ="1024MB" |
                 .resource_limits.kernel_space_stack_size ="4MB" |
                 .resource_limits.max_num_of_threads = 96 |
                 .entry_points = [ "/opt/ltp" ] |
