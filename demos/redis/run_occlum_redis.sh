b64b04995f2e933f6ebe9d62866e14366c82b985
zongmin.gu <zongmin.gzm@antgroup.com>
2021-12-10 13:57:39 +0800

    [test] fix the redis demo

--- demos/redis/run_occlum_redis.sh
+++ demos/redis/run_occlum_redis.sh
@@ -18,4 +18,4 @@ copy_bom -f $bomfile --root image --include-dir /opt/occlum/etc/template
 
 occlum build
 # 3. Run redis server
-occlum run /bin/redis-server --save "" --appendonly no &
+occlum run /bin/redis-server /etc/redis.conf &
