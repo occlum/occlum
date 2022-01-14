b40408cb91ac5b44ce963587a3f65fb08ad4b463
LI Qing <geding.lq@antgroup.com>
2022-01-14 10:01:55 +0800

    [libos] Change ROOT_INODE to ROOT_FS
    Filesystem should be the owner of its root inode

--- src/libos/src/fs/fs_view.rs
+++ src/libos/src/fs/fs_view.rs
@@ -165,17 +165,19 @@ impl FsView {
         if path.len() > 0 && path.as_bytes()[0] == b'/' {
             // absolute path
             let abs_path = path.trim_start_matches('/');
-            let inode = ROOT_INODE
+            let inode = ROOT_FS
                 .read()
                 .unwrap()
+                .root_inode()
                 .lookup_follow(abs_path, MAX_SYMLINKS)?;
             Ok(inode)
         } else {
             // relative path
             let cwd = self.cwd().trim_start_matches('/');
-            let inode = ROOT_INODE
+            let inode = ROOT_FS
                 .read()
                 .unwrap()
+                .root_inode()
                 .lookup_follow(cwd, MAX_SYMLINKS)?
                 .lookup_follow(path, MAX_SYMLINKS)?;
             Ok(inode)
