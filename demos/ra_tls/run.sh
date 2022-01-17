30b5b767847b2878ffd3eba4f270ea0d51542c46
yuanwu <yuan.wu@intel.com>
2022-01-17 12:42:25 +0800

    Enable the grpc ra-tls demo
    Signed-off-by: yuanwu <yuan.wu@intel.com>

---
+++ demos/ra_tls/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+postfix=$1
+
+if [ "$postfix" != "server" ] && [ "$postfix" != "client" ]; then
+    echo "input error args, it should be:" 
+    echo "./run.sh server"
+    echo "./run.sh client"
+    exit 1
+fi
+
+pushd occlum_instance_$postfix
+occlum run /bin/$postfix
+popd
