8303041e61618fe97675562a9d3202041f350062
zongmin.gu <zongmin.gzm@antgroup.com>
2022-01-19 19:04:51 +0800

    [test] update flink demo ignore file

--- demos/flink/download_flink.sh
+++ demos/flink/download_flink.sh
@@ -1,4 +1,4 @@
 apt-get update
 apt-get install -y openjdk-11-jdk
-wget https://archive.apache.org/dist/flink/flink-1.11.3/flink-1.11.3-bin-scala_2.11.tgz
-tar -xvzf flink-1.11.3-bin-scala_2.11.tgz
+[ -f flink-1.11.3-bin-scala_2.11.tgz ] || wget https://archive.apache.org/dist/flink/flink-1.11.3/flink-1.11.3-bin-scala_2.11.tgz
+[ -d flink-1.11.3 ] || tar -xvzf flink-1.11.3-bin-scala_2.11.tgz
