97b47d3d61c6d7a07d495eae9c3064e6667ad330
Zheng, Qi <huaiqing.zq@antgroup.com>
2022-01-21 17:12:40 +0800

    [ci] Clean up the gRPC RATLS demo and add musl version

--- demos/ra_tls/build_occlum_instance.sh
+++ demos/ra_tls/build_occlum_instance.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-occlum_glibc=/opt/occlum/glibc/lib/
 set -ex
 
 get_mr() {
-    sgx_sign dump -enclave ../occlum_instance_$1/build/lib/libocclum-libos.signed.so -dumpfile ../metadata_info_$1.txt
+    sgx_sign dump -enclave ../occlum_$1/build/lib/libocclum-libos.signed.so -dumpfile ../metadata_info_$1.txt
     if [ "$2" == "mr_enclave" ]; then
         sed -n -e '/enclave_hash.m/,/metadata->enclave_css.body.isv_prod_id/p' ../metadata_info_$1.txt |head -3|tail -2|xargs|sed 's/0x//g'|sed 's/ //g'
     elif [ "$2" == "mr_signer" ]; then
@@ -13,40 +12,47 @@ get_mr() {
 
 build_instance() {
     # 1. Init Occlum Workspace
-    rm -rf occlum_instance_$postfix
-    mkdir occlum_instance_$postfix
-    pushd occlum_instance_$postfix
+    rm -rf occlum_$postfix
+    mkdir occlum_$postfix
+    pushd occlum_$postfix
     occlum init
-    new_json="$(jq '.resource_limits.user_space_size = "320MB" |
+    new_json="$(jq '.resource_limits.user_space_size = "500MB" |
                     .process.default_mmap_size = "256MB"' Occlum.json)" && \
     echo "${new_json}" > Occlum.json
-    # 2. Copy files into Occlum Workspace and Build
-    #cp ../dynamic_config.json image/etc/dynamic_config.json
-    cp ../dynamic_config.json image/dynamic_config.json
-    #cp ../dynamic_config_$postfix.json image/dynamic_config.json
+
+    if [ "$postfix" == "server" ]; then
+        bomfile="../grpc_ratls_server.yaml"
+    else
+        bomfile="../grpc_ratls_client.yaml"
+    fi
+
+    rm -rf image
+    copy_bom -f $bomfile --root image --include-dir /opt/occlum/etc/template
+
     if [ "$postfix" == "server" ]; then
         jq ' .verify_mr_enclave = "on" |
              .verify_mr_signer = "on" |
 	     .sgx_mrs[0].mr_enclave = ''"'`get_mr client mr_enclave`'" |
 	     .sgx_mrs[0].mr_signer = ''"'`get_mr client mr_signer`'" ' ../dynamic_config.json > image/dynamic_config.json 
+    
+        if [ "$libnss_require" == "y" ]; then
+            cp /lib/x86_64-linux-gnu/libnss*.so.2 image/$occlum_glibc
+            cp /lib/x86_64-linux-gnu/libresolv.so.2 image/$occlum_glibc
+        fi
     fi
-    mkdir -p image/usr/share/grpc
-    cp -rf /share/grpc/*  image/usr/share/grpc/
-    cp $occlum_glibc/libdl.so.2 image/$occlum_glibc
-    cp $occlum_glibc/librt.so.1 image/$occlum_glibc
-    cp $occlum_glibc/libm.so.6 image/$occlum_glibc
-    cp /lib/x86_64-linux-gnu/libtinfo.so.5 image/$occlum_glibc
-    cp /lib/x86_64-linux-gnu/libnss*.so.2 image/$occlum_glibc
-    cp /lib/x86_64-linux-gnu/libresolv.so.2 image/$occlum_glibc
-    cp -rf /etc/hostname image/etc/
-    cp -rf /etc/ssl image/etc/
-    cp -rf /etc/passwd image/etc/
-    cp -rf /etc/group image/etc/
-    cp -rf /etc/nsswitch.conf image/etc/
-    cp -rf /grpc/examples/cpp/ratls/build/* image/bin/
+
     occlum build
     popd
 }
+
+if [[ $1 == "musl" ]]; then
+    echo "*** Build and musl-libc Occlum instance ***"
+else
+    echo "*** Build and run glibc Occlum instance ***"
+    # glibc version requires libnss
+    libnss_require="y"
+    occlum_glibc=/opt/occlum/glibc/lib/
+fi
 
 postfix=client
 build_instance
---
+++ demos/ra_tls/download_and_prepare.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+# Download and update cmake
+rm -rf cmake-3.20.2*
+wget https://github.com/Kitware/CMake/releases/download/v3.20.2/cmake-3.20.2.tar.gz
+tar -zxvf cmake-3.20.2.tar.gz
+pushd cmake-3.20.2
+./bootstrap
+make install
+popd
+
+# GRPC env
+GRPC_VERSION=v1.38.x
+GRPC_PATH=grpc-src
+
+# GRPC source code
+rm -rf ${GRPC_PATH}
+git clone https://github.com/grpc/grpc -b ${GRPC_VERSION} ${GRPC_PATH}
+pushd ${GRPC_PATH} \
+    && git checkout v1.38.1 \
+    && git submodule update --init
+popd
+
+# Occlum source code
+rm -rf occlum
+git clone  https://github.com/occlum/occlum
+pushd occlum
+make submodule
+popd
+
+
