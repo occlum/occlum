b62b5e23ebe74865170890b90be781806b7a87eb
zongmin.gu <zongmin.gzm@antgroup.com>
2022-03-08 14:37:24 +0800

    Remove a broken symlink in openjdk 11

--- demos/flink/download_flink.sh
+++ demos/flink/download_flink.sh
@@ -1,4 +1,7 @@
 apt-get update
-apt-get install -y openjdk-11-jdk
+apt-get install -y openjdk-11-jre
+#The openjdk has a broken symlink, remove it as a workaround
+rm /usr/lib/jvm/java-11-openjdk-amd64/lib/security/blacklisted.certs
+
 wget https://archive.apache.org/dist/flink/flink-1.10.1/flink-1.10.1-bin-scala_2.11.tgz
 tar -xvzf flink-1.10.1-bin-scala_2.11.tgz
