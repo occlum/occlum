c7712fb64ddaa3e869fcad88653dd362b772e742
Shaowei Song <1498430017@qq.com>
2022-04-01 14:32:39 +0800

    [demo] Add RocksDB demo and benchmark test

---
+++ demos/rocksdb/dl_and_build_rocksdb.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+BLUE='\033[1;34m'
+NC='\033[0m'
+echo -e "${BLUE}Start installing dependencies.${NC}"
+
+DEPS="libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev"
+
+apt-get update
+apt-get install -y $DEPS
+echo -e "${BLUE}Finish installing dependencies.${NC}"
+
+echo -e "${BLUE}Start building rocksdb from src.${NC}"
+TAG="v7.0.4"
+
+rm -rf rocksdb
+git clone -b $TAG https://github.com/facebook/rocksdb.git
+pushd rocksdb
+
+# Disable fallocate(different st_blksize will cause SIGFPE) and file_sync_range(not implemented) syscalls
+sed -i '2467 i CXXFLAGS += -UROCKSDB_FALLOCATE_PRESENT -UROCKSDB_RANGESYNC_PRESENT' Makefile
+
+# Build librocksdb.a and db_bench tool
+CFLAGS="-O2 -fPIC" CXXFLAGS="-O2 -fPIC" LDFLAGS="-pie" \
+make -j$(nproc) static_lib db_bench DEBUG_LEVEL=0
+echo -e "${BLUE}Finish building rocksdb from src.${NC}"
+
+echo -e "${BLUE}Start building simple_rocksdb_example.${NC}"
+pushd examples
+EXAMPLE=compaction_filter_example
+g++ -O2 -std=c++17 -fPIC -pie -fno-rtti $EXAMPLE.cc -osimple_rocksdb_example ../librocksdb.a -I../include \
+-lpthread -lrt -ldl -lgflags -lsnappy -lz -lbz2 -llz4 -lzstd
+echo -e "${BLUE}Finish building simple_rocksdb_example.${NC}"
+
+./simple_rocksdb_example
+popd
+
+popd
---
+++ demos/rocksdb/run_benchmark.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# 1. Init Occlum Workspace
+rm -rf occlum_instance && occlum new occlum_instance
+cd occlum_instance
+
+# 2. Copy files into Occlum Workspace and build
+rm -rf image
+copy_bom -f ../rocksdb.yaml --root image --include-dir /opt/occlum/etc/template
+
+new_json="$(jq '.resource_limits.user_space_size = "3000MB" |
+                .resource_limits.kernel_space_heap_size ="1024MB" |
+                .resource_limits.kernel_space_stack_size ="4MB" |
+                .resource_limits.max_num_of_threads = 96' Occlum.json)" && \
+echo "${new_json}" > Occlum.json
+
+occlum build
+
+# 3. Run example and benchmark with config
+BLUE='\033[1;34m'
+NC='\033[0m'
+echo -e "${BLUE}Run simple_rocksdb_example in Occlum.${NC}"
+occlum run /bin/simple_rocksdb_example
+
+echo -e "${BLUE}Run benchmark in Occlum.${NC}"
+
+# More benchmark config at https://github.com/facebook/rocksdb/wiki/Benchmarking-tools
+BENCHMARK_CONFIG="fillseq,fillrandom,readseq,readrandom,deleteseq"
+occlum run /bin/db_bench --benchmarks=${BENCHMARK_CONFIG}
+
+echo -e "${BLUE}Run benchmark in host.${NC}"
+cd ../rocksdb && ./db_bench --benchmarks=$BENCHMARK_CONFIG
