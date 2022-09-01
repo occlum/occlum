ClawSeven/ngo

[async-rt] Add the new scheduler

src/libos/crates/async-io/src/event/event_counter.rs
@@ -87,7 +87,7 @@ mod tests {
             // Make sure that the child executes first
             let _20ms = std::time::Duration::from_millis(20);
             std::thread::sleep(_20ms);
-            async_rt::sched::yield_().await;
+            async_rt::scheduler::yield_().await;
 
             // Wake up the child task
             counter.write();

src/libos/crates/async-rt/src/sync/mod.rs
@@ -15,11 +15,11 @@ pub use rwlock::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 #[cfg(test)]
 mod tests {
     use crate::prelude::*;
-    const TEST_PARALLELISM: u32 = 4;
+    const TEST_VPUS: u32 = 4;
 
     #[ctor::ctor]
     fn auto_init_executor() {
-        crate::config::set_parallelism(TEST_PARALLELISM);
+        crate::vcpu::set_total(TEST_VPUS);
     }
 
     #[test]
@@ -203,7 +203,7 @@ mod tests {
                 if counter.load(Ordering::Relaxed) == 3 {
                     break;
                 }
-                crate::sched::yield_().await;
+                crate::scheduler::yield_().await;
             }
         }
 

src/libos/crates/async-rt/src/wait/mod.rs
@@ -143,7 +143,7 @@ mod tests {
                 assert!(waiter.wait_timeout(Some(&mut timeout)).await.is_ok());
             });
 
-            crate::sched::yield_().await;
+            crate::scheduler::yield_().await;
             std::thread::sleep(Duration::from_millis(sleep_time));
             waker.wake();
             join_handle.await;
@@ -160,7 +160,7 @@ mod tests {
                 waiter.wait_timeout::<Duration>(None).await.unwrap();
             });
 
-            crate::sched::yield_().await;
+            crate::scheduler::yield_().await;
             waker.wake();
             join_handle.await;
         });

src/libos/src/process/do_exec.rs
@@ -121,7 +121,7 @@ async fn wait_for_other_threads_to_exit(current_ref: &ThreadRef) {
         notify_all_threads_to_exit(current_ref.process());
 
         // Must yield here for other threads to run
-        async_rt::sched::yield_().await;
+        async_rt::scheduler::yield_().await;
 
         let thread_num = current_ref.process().threads().len();
         if thread_num == 1 {

occlum/occlum

src/libos/crates/async-io/src/event/event_counter.rs
@@ -87,7 +87,7 @@ mod tests {
             // Make sure that the child executes first
             let _20ms = std::time::Duration::from_millis(20);
             std::thread::sleep(_20ms);
-            async_rt::sched::yield_().await;
+            async_rt::scheduler::yield_now().await;
 
             // Wake up the child task
             counter.write();

src/libos/crates/async-rt/src/sync/mod.rs
@@ -15,11 +15,11 @@ pub use rwlock::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 #[cfg(test)]
 mod tests {
     use crate::prelude::*;
-    const TEST_PARALLELISM: u32 = 4;
+    const TEST_VPUS: u32 = 4;
 
     #[ctor::ctor]
     fn auto_init_executor() {
-        crate::config::set_parallelism(TEST_PARALLELISM);
+        crate::vcpu::set_total(TEST_VPUS);
     }
 
     #[test]
@@ -203,7 +203,7 @@ mod tests {
                 if counter.load(Ordering::Relaxed) == 3 {
                     break;
                 }
-                crate::sched::yield_().await;
+                crate::scheduler::yield_now().await;
             }
         }
 

src/libos/crates/async-rt/src/wait/mod.rs
@@ -143,7 +143,7 @@ mod tests {
                 assert!(waiter.wait_timeout(Some(&mut timeout)).await.is_ok());
             });
 
-            crate::sched::yield_().await;
+            crate::scheduler::yield_now().await;
             std::thread::sleep(Duration::from_millis(sleep_time));
             waker.wake();
             join_handle.await;
@@ -160,7 +160,7 @@ mod tests {
                 waiter.wait_timeout::<Duration>(None).await.unwrap();
             });
 
-            crate::sched::yield_().await;
+            crate::scheduler::yield_now().await;
             waker.wake();
             join_handle.await;
         });

src/libos/src/process/do_exec.rs
@@ -121,7 +121,7 @@ async fn wait_for_other_threads_to_exit(current_ref: &ThreadRef) {
         notify_all_threads_to_exit(current_ref.process());
 
         // Must yield here for other threads to run
-        async_rt::sched::yield_().await;
+        async_rt::scheduler::yield_now().await;
 
         let thread_num = current_ref.process().threads().len();
         if thread_num == 1 {