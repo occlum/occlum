7bbe7b65fe947edbb01ff1248acafd983aeb296b
Hui, Chunyang <sanqian.hcy@antfin.com>
2022-09-05 07:42:29 +0000

    Update Vault demo to v1.10.0

--- demos/golang/vault/prepare_vault.sh
+++ demos/golang/vault/prepare_vault.sh
@@ -3,15 +3,16 @@ set -e
 
 src_dir="./source_code"
 vault="$src_dir/bin/vault"
+vault_version="1.10.0"
 
 if [ -f "$vault" ]; then
     echo "Warning: the current working directory has Vault already downloaded and built"
     exit 1
 fi
 
-# download the source code of Vault v1.7.0
-wget https://github.com/hashicorp/vault/archive/refs/tags/v1.7.0.tar.gz
-mkdir -p $src_dir && tar -xvzf v1.7.0.tar.gz -C $src_dir --strip-components=1
+# download the source code of Vault
+wget https://github.com/hashicorp/vault/archive/refs/tags/v"$vault_version".tar.gz
+mkdir -p $src_dir && tar -xvzf v"$vault_version".tar.gz -C $src_dir --strip-components=1
 
 # build Vault executable
 pushd $src_dir
