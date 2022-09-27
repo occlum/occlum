edb52cb031062b5a0345058054d71c494f6a6cb2
Jianfeng Jiang <jiangjianfeng.jjf@antgroup.com>
2022-09-27 14:17:19 +0800

    fix copy_bom error message if rsync is not installed

--- tools/copy_bom/src/error.rs
+++ tools/copy_bom/src/error.rs
@@ -1,4 +1,4 @@
-/// This file defines error number to indicate different errors
+//! This file defines error number to indicate different errors
 pub static FILE_NOT_EXISTS_ERROR: i32 = -1;
 pub static COPY_FILE_ERROR: i32 = -2;
 pub static INVALID_BOM_FILE_ERROR: i32 = -3;
@@ -7,3 +7,4 @@ pub static CREATE_SYMLINK_ERROR: i32 = -5;
 pub static COPY_DIR_ERROR: i32 = -6;
 pub static INCORRECT_HASH_ERROR: i32 = -7;
 pub static MISSING_LIBRARY_ERROR: i32 = -8;
+pub static RSYNC_NOT_FOUND_ERROR: i32 = -9;
