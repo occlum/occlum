526bbc8b821d5dc3ed3178c4b568a95549ea1ebc
Zheng, Qi <huaiqing.zq@antgroup.com>
2022-10-26 16:58:06 +0800

    Set conda python version as 3.8.10 to workaround non-PIE issues

--- demos/python/python_glibc/install_python_with_conda.sh
+++ demos/python/python_glibc/install_python_with_conda.sh
@@ -8,5 +8,5 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd )"
 # 2. Install python and dependencies to specified position
 [ -f Miniconda3-latest-Linux-x86_64.sh ] || wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 [ -d miniconda ] || bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $script_dir/miniconda
-$script_dir/miniconda/bin/conda create --prefix $script_dir/python-occlum -y python=3.8 numpy=1.21.5 scipy=1.7.3 scikit-learn=1.0 pandas=1.3 Cython
+$script_dir/miniconda/bin/conda create --prefix $script_dir/python-occlum -y python=3.8.10 numpy=1.21.5 scipy=1.7.3 scikit-learn=1.0 pandas=1.3 Cython
 
