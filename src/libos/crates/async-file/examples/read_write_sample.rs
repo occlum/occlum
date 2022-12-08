458ff545942e6e82063f13538e83263691800795
LI Qing <geding.lq@antgroup.com>
2022-12-08 09:16:11 +0800

    [crates] Remove the async-file crate

--- src/libos/crates/async-file/examples/read_write_sample.rs
+++
@@ -1,7 +0,0 @@
-include!("common/sample.rs");
-
-fn main() {
-    init_async_rt();
-
-    read_write_sample();
-}
--- src/libos/crates/async-file/src/page_cache/page_state.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum PageState {
-    Uninit,
-    UpToDate,
-    Dirty,
-    Fetching,
-    Flushing,
-}
