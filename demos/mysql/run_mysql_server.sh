25c7e6591e8637db15a2be9450bda40798ed7700
Shaowei Song <1498430017@qq.com>
2022-12-14 16:25:22 +0800

    [demos] Add MySQL demo

---
+++ demos/mysql/run_mysql_server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+bomfile=${SCRIPT_DIR}/mysql.yaml
+
+MYSQL=mysql
+MYSQLD=mysqld
+
+# 1. Init Occlum instance
+rm -rf occlum_instance && occlum new occlum_instance
+pushd occlum_instance
+
+yq '.resource_limits.user_space_size = "8000MB" |
+    .resource_limits.kernel_space_heap_size ="1000MB" ' -i Occlum.yaml
+
+# 2. Copy files into Occlum instance and build
+rm -rf image
+copy_bom -f $bomfile --root image --include-dir /opt/occlum/etc/template
+
+occlum build
+
+# 3. Run the program
+echo -e "${GREEN}Run mysql server (mysqld) on Occlum${NC}"
+
+occlum start
+
+echo -e "${GREEN}mysql server initialize${NC}"
+
+occlum exec /bin/${MYSQLD} --initialize-insecure --user=root
+
+echo -e "${GREEN}mysql server start${NC}"
+
+occlum exec /bin/${MYSQLD} --user=root
+
+popd
