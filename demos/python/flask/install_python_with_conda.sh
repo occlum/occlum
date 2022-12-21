d7d485de276524d573a7e46951d80bdfc6625bfa
Hui, Chunyang <sanqian.hcy@antfin.com>
2022-12-21 16:08:26 +0800

    Fix flask demo failure

--- demos/python/flask/install_python_with_conda.sh
+++ demos/python/flask/install_python_with_conda.sh
@@ -8,4 +8,4 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd )"
 # 2. Install python and dependencies to specified position
 [ -f Miniconda3-latest-Linux-x86_64.sh ] || wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 [ -d miniconda ] || bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $script_dir/miniconda
-$script_dir/miniconda/bin/conda create --prefix $script_dir/python-occlum -y python=3.7.11 flask=1.1.2 flask-restful=0.3.8 jinja2=3.0
+$script_dir/miniconda/bin/conda create --prefix $script_dir/python-occlum -y python=3.7.11 flask=1.1.2 flask-restful=0.3.8 jinja2=3.0  werkzeug=2.0.3
