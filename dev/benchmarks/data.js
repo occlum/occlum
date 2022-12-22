e7209da7446a4f77431d1256826ca514185b9a97
github-action-benchmark <github@users.noreply.github.com>
2022-12-22 11:12:03 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 3723b5c23c8aab7d96285c5f5646bf3f07213d28

---
+++ dev/benchmarks/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1671678723630,
+  "repoUrl": "https://github.com/occlum/occlum",
+  "entries": {
+    "Sysbench Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "huaiqing.zq@antgroup.com",
+            "name": "Zheng, Qi",
+            "username": "qzheng527"
+          },
+          "committer": {
+            "email": "volcano.dr@hotmail.com",
+            "name": "volcano",
+            "username": "volcano0dr"
+          },
+          "distinct": true,
+          "id": "3723b5c23c8aab7d96285c5f5646bf3f07213d28",
+          "message": "[ci] enable push tigger for benchmark ci",
+          "timestamp": "2022-12-22T10:37:38+08:00",
+          "tree_id": "89d17ca6a4a547c570f5f7e3647de9a35a0905a9",
+          "url": "https://github.com/occlum/occlum/commit/3723b5c23c8aab7d96285c5f5646bf3f07213d28"
+        },
+        "date": 1671678721461,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.09,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 327.53,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 61712.05,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 320.17,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      }
+    ]
+  }
+}
