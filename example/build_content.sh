a0c37b038c59d098611437aa491e5e74e3a70999
Zheng, Qi <huaiqing.zq@antgroup.com>
2023-02-28 17:12:19 +0800

    [example] Update tf example with grpc ratls libs

--- example/build_content.sh
+++ example/build_content.sh
@@ -3,22 +3,14 @@ set -e
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd )"
 
-export DEP_LIBS_DIR="${script_dir}/dep_libs"
 export INITRA_DIR="${script_dir}/init_ra"
-export RATLS_DIR="${script_dir}/../demos/ra_tls"
+export RATLS_DIR="${script_dir}/../tools/toolchains/grpc_ratls"
 export TF_DIR="${script_dir}/tf_serving"
 
 function build_ratls()
 {
-    rm -rf ${DEP_LIBS_DIR} && mkdir ${DEP_LIBS_DIR}
     pushd ${RATLS_DIR}
-    ./download_and_prepare.sh
-    ./build_and_install.sh musl
-    ./build_occlum_instance.sh musl
-
-    cp ./grpc-src/examples/cpp/ratls/build/libgrpc_ratls_client.so ${DEP_LIBS_DIR}/
-    cp ./grpc-src/examples/cpp/ratls/build/libhw_grpc_proto.so ${DEP_LIBS_DIR}/
-
+    ./build.sh
     popd
 }
 
