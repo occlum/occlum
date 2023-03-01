0ac398b635438fdbd2170861377d908ea9295daa
LI Qing <geding.lq@antgroup.com>
2023-03-01 11:27:39 +0800

    Add support to statfs on hostfs

--- src/libos/src/fs/fs_ops/mod.rs
+++ src/libos/src/fs/fs_ops/mod.rs
@@ -5,7 +5,7 @@ pub use self::getcwd::do_getcwd;
 pub use self::mount::{
     do_mount, do_mount_rootfs, do_umount, MountFlags, MountOptions, UmountFlags,
 };
-pub use self::statfs::{do_fstatfs, do_statfs, Statfs};
+pub use self::statfs::{do_fstatfs, do_statfs, fetch_host_statfs, Statfs};
 pub use self::sync::do_sync;
 
 mod chdir;
