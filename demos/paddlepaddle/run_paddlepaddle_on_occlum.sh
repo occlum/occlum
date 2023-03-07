2a54d87e95e208d3fcf32f3795379b55b51cf087
sunhe05 <sunhe05@baidu.com>
2023-03-07 02:47:45 +0000

    Add PaddlePaddle demo

---
+++ demos/paddlepaddle/run_paddlepaddle_on_occlum.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd )"
+python_dir="$script_dir/occlum_instance/image/opt/python-occlum"
+
+cd occlum_instance && rm -rf image
+copy_bom -f ../paddlepaddle.yaml --root image --include-dir /opt/occlum/etc/template
+
+if [ ! -d $python_dir ];then
+    echo "Error: cannot stat '$python_dir' directory"
+    exit 1
+fi
+
+yq '.resource_limits.user_space_size.init = "6000MB" |
+    .resource_limits.kernel_space_heap_size.init = "256MB" |
+    .env.default += ["PYTHONHOME=/opt/python-occlum"]' -i Occlum.yaml
+
+occlum build
+
+# Run the python demo
+echo -e "${BLUE}occlum run /bin/python3 demo.py${NC}"
+occlum run /bin/python3 demo.py
