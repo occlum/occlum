5e739bbf9fc5a17f2274c0a476b0e3719ce84f62
Zheng, Qi <huaiqing.zq@antgroup.com>
2023-03-10 15:26:47 +0800

    Modify the project license description

--- src/libos/crates/async-rt/src/sync/rwlock/inner.rs
+++ src/libos/crates/async-rt/src/sync/rwlock/inner.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Ant Financial Services Group. All rights reserved.
+// Copyright (C) 2019 - 2023 The Occlum Project. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -10,7 +10,7 @@
 //    notice, this list of conditions and the following disclaimer in
 //    the documentation and/or other materials provided with the
 //    distribution.
-//  * Neither the name of Ant Financial Services Group nor the names
+//  * Neither the name of copyright holder nor the names
 //    of its contributors may be used to endorse or promote products
 //    derived from this software without specific prior written
 //    permission.
