35c715dd458c17acdad65c2c47b8fb93c965fd5a
Zheng, Qi <huaiqing.zq@antgroup.com>
2023-03-20 10:51:41 +0800

    [toolchain] Add AECS client libaries build and install in toolchain

---
+++ tools/toolchains/aecs_client/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd )"
+INSTALL_DIR=/opt/occlum/toolchains/aecs_client
+AECS_DIR=${script_dir}/enclave-configuration-service
+
+git clone https://github.com/SOFAEnclave/enclave-configuration-service.git
+
+pushd ${AECS_DIR}
+git submodule update --init --recursive
+
+echo "Start building AECS client libraries ..."
+pushd client/cpp_occlum
+./occlum_build_prepare.sh
+./occlum_build_aecs_client.sh --teetype SGX2 --envtype OCCLUM
+
+echo "Move AECS client libraries to toolchain path"
+mkdir -p ${INSTALL_DIR}
+cp ./build/out/libaecs_client.so ${INSTALL_DIR}/
+cp ./build/out/libual.so ${INSTALL_DIR}/
+cp /usr/local/occlum/x86_64-linux-gnu/lib/libcurl_static.a ${INSTALL_DIR}/
+popd
+
+# Clean up
+rm -rf /usr/local/occlum/x86_64-linux-gnu
+rm -rf /opt/occlum/toolchains/gcc/x86_64-linux-gnu
+
+popd
