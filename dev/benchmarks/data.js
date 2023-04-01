084eb0ac5361fae350382daf0e7ddfdce2322d11
github-action-benchmark <github@users.noreply.github.com>
2023-04-02 06:23:25 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 6cb368fbbeccae27f61677f70a8166772a30b528

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680387702559,
+  "lastUpdate": 1680387805527,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -1488,6 +1488,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 262.64,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "zhubojun",
+            "username": "Bonjourz",
+            "email": "zhubojun.zbj@antgroup.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "6cb368fbbeccae27f61677f70a8166772a30b528",
+          "message": "[libos] Retain red zone area and FLAGS unmodified across syscall\n\n1. The previous implementation of __occlum_syret usse the red zone to\nsave the register temporarily, which may overwrite the red zone area.\nThis change avoids using the red zone when handling syscall.\n\n2. Save and restore the FLAGS register before and after the operations\non PKRU to keep the FLAGS unmodified.",
+          "timestamp": "2023-03-21T01:46:38Z",
+          "url": "https://github.com/occlum/occlum/commit/6cb368fbbeccae27f61677f70a8166772a30b528"
+        },
+        "date": 1680387803449,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.08,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 54.14,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 11141.66,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 257.95,
             "unit": "ms",
             "extra": "per95"
           }
