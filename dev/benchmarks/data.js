311f58d353335d5d88a0afabffb1ebea38937988
github-action-benchmark <github@users.noreply.github.com>
2023-04-06 06:19:30 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 7181abd390994f362c56cb00ef267e3752aa9b39

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680733069218,
+  "lastUpdate": 1680733170800,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -1534,6 +1534,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 257.95,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Zheng, Qi",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "7181abd390994f362c56cb00ef267e3752aa9b39",
+          "message": "[demos] Specify the sgx/occlum apt packages version",
+          "timestamp": "2023-03-31T06:54:44Z",
+          "url": "https://github.com/occlum/occlum/commit/7181abd390994f362c56cb00ef267e3752aa9b39"
+        },
+        "date": 1680733169451,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.08,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 50.3,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 11578.2,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 240.02,
             "unit": "ms",
             "extra": "per95"
           }
