5fb1876e8b887a7ce255f95090d456e1decbc2d5
github-action-benchmark <github@users.noreply.github.com>
2023-04-09 06:09:35 +0800

    add FIO Benchmark on AsyncSFS and JinDisk (customBiggerIsBetter) benchmark result for 7181abd390994f362c56cb00ef267e3752aa9b39

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680733170800,
+  "lastUpdate": 1680991775585,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -3804,6 +3804,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 144,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Zheng, Qi",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "7181abd390994f362c56cb00ef267e3752aa9b39",
+          "message": "[demos] Specify the sgx/occlum apt packages version",
+          "timestamp": "2023-03-31T06:54:44Z",
+          "url": "https://github.com/occlum/occlum/commit/7181abd390994f362c56cb00ef267e3752aa9b39"
+        },
+        "date": 1680991773815,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 356,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 180,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 160,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 140,
             "unit": "MiB/s",
             "extra": "randread"
           }
