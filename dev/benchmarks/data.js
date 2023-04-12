339309b7b09da80080ebbe19f077248da0251fab
github-action-benchmark <github@users.noreply.github.com>
2023-04-13 06:16:57 +0800

    add FIO Benchmark on AsyncSFS and JinDisk (customBiggerIsBetter) benchmark result for fca61640198e19bdfae9e267c95235f45112aeb8

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681337438434,
+  "lastUpdate": 1681337817390,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -4056,6 +4056,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 140,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "envlearner",
+            "username": "envlearner",
+            "email": "yishan_li@hust.edu.cn"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "fca61640198e19bdfae9e267c95235f45112aeb8",
+          "message": "Fix some typo errors\nincluding both doc/comment typo errors and code typo errors",
+          "timestamp": "2023-04-11T06:19:43Z",
+          "url": "https://github.com/occlum/occlum/commit/fca61640198e19bdfae9e267c95235f45112aeb8"
+        },
+        "date": 1681337815932,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 325,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 261,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 141,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 132,
             "unit": "MiB/s",
             "extra": "randread"
           }
