b3b7613947ccd401f43306b90d822f5831fdb560
Hui, Chunyang <sanqian.hcy@antfin.com>
2023-04-12 10:00:13 +0000

    [vdso-time] Add support until Linux 6.2

--- src/libos/crates/vdso-time/src/sys.rs
+++ src/libos/crates/vdso-time/src/sys.rs
@@ -122,7 +122,7 @@ pub enum VdsoDataPtr {
     V5_3(*const vdso_data_v5_3),
     // === Linux 5.6 - 5.8 ===
     V5_6(*const vdso_data_v5_6),
-    // === Linux 5.9 - 5.12 ===
+    // === Linux 5.9 - 6.2 ===
     V5_9(*const vdso_data_v5_9),
 }
 
@@ -521,7 +521,7 @@ impl VdsoData for vdso_data_v5_6 {
     }
 }
 
-// === Linux 5.9 - 5.18 ===
+// === Linux 5.9 - 5.19, 6.0 - 6.2 ===
 // struct vdso_data
 
 #[repr(C)]
