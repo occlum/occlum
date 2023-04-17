4cbf728910a816a6d03e31ab701eef439752f485
Zheng, Qi <huaiqing.zq@antgroup.com>
2023-04-17 19:35:19 +0800

    [toolchain] Add get key to buffer API for grpc_ratls

--- tools/toolchains/grpc_ratls/ra_tls/grpc/v1.38.1/examples/cpp/ratls/grpc_ratls_client.h
+++ tools/toolchains/grpc_ratls/ra_tls/grpc/v1.38.1/examples/cpp/ratls/grpc_ratls_client.h
@@ -13,6 +13,14 @@ extern int grpc_ratls_get_secret(
     const char *secret_file // secret file to be saved
 );
 
+extern int grpc_ratls_get_secret_to_buf(
+    const char *server_addr, // grpc server address+port, such as "localhost:50051"
+    const char *config_json, // ratls handshake config json file
+    const char *name, // secret name to be requested
+    char *secret_buf, // buffer to save secret
+    unsigned int *buf_len // buffer size
+);
+
 #ifdef __cplusplus
 }
 #endif
