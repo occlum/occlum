2c966dd36ebe4096d762f8153fbfbe92d8ec2ace
Zheng, Qi <huaiqing.zq@antgroup.com>
2023-06-05 14:25:41 +0800

    Bump version to 0.29.6

--- src/pal/include/occlum_version.h
+++ src/pal/include/occlum_version.h
@@ -3,10 +3,10 @@
 #ifndef _OCCLUM_VERSION_H_
 #define _OCCLUM_VERSION_H_
 
-// Version = 0.29.5
+// Version = 0.29.6
 #define OCCLUM_MAJOR_VERSION    0
 #define OCCLUM_MINOR_VERSION    29
-#define OCCLUM_PATCH_VERSION    5
+#define OCCLUM_PATCH_VERSION    6
 
 #define STRINGIZE_PRE(X) #X
 #define STRINGIZE(X) STRINGIZE_PRE(X)
