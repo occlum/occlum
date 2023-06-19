c2c71c97a0d3aa5c310e52126265c97fe166b8bc
Zheng, Qi <huaiqing.zq@antgroup.com>
2023-06-19 10:21:25 +0800

    [tools] Do init-ra build according to TEE type

--- tools/toolchains/aecs_client/build.sh
+++ tools/toolchains/aecs_client/build.sh
@@ -5,6 +5,9 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd )"
 INSTALL_DIR=/opt/occlum/toolchains/aecs_client
 AECS_DIR=${script_dir}/enclave-configuration-service
 
+# Default TEE TYPE is SGX2, also support HYPERENCLAVE
+TEETYPE=${1:-SGX2}
+
 git clone https://github.com/SOFAEnclave/enclave-configuration-service.git
 
 pushd ${AECS_DIR}
@@ -13,7 +16,7 @@ git submodule update --init --recursive
 echo "Start building AECS client libraries ..."
 pushd client/cpp_occlum
 ./occlum_build_prepare.sh
-./occlum_build_aecs_client.sh --teetype SGX2 --envtype OCCLUM
+./occlum_build_aecs_client.sh --teetype ${TEETYPE} --envtype OCCLUM
 
 echo "Move AECS client libraries to toolchain path"
 mkdir -p ${INSTALL_DIR}
