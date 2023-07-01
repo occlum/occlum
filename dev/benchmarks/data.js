b2325720ebb5b39e2eaf6a1cdd3d5664ed695e3c
github-action-benchmark <github@users.noreply.github.com>
2023-07-02 06:15:03 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for e2f7e6109a3074e64730a950e62b1037d9e5e17c

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688249626060,
+  "lastUpdate": 1688249703334,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -5444,6 +5444,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 2587,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shaowei Song",
+            "username": "lucassong-mh",
+            "email": "1498430017@qq.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "e2f7e6109a3074e64730a950e62b1037d9e5e17c",
+          "message": "[vm] Minor fix in debug asserts in shm",
+          "timestamp": "2023-06-26T11:28:39Z",
+          "url": "https://github.com/occlum/occlum/commit/e2f7e6109a3074e64730a950e62b1037d9e5e17c"
+        },
+        "date": 1688249700258,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 2037,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 2037,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
