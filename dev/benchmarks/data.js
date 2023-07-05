ffd56f7c978cc0a94c7f4b444dad52f751f68c8c
github-action-benchmark <github@users.noreply.github.com>
2023-07-06 06:18:40 +0800

    add FIO Benchmark on SEFS (customBiggerIsBetter) benchmark result for e2f7e6109a3074e64730a950e62b1037d9e5e17c

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688595112305,
+  "lastUpdate": 1688595520197,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -6848,6 +6848,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 184,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shaowei Song",
+            "username": "lucassong-mh",
+            "email": "1498430017@qq.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "e2f7e6109a3074e64730a950e62b1037d9e5e17c",
+          "message": "[vm] Minor fix in debug asserts in shm",
+          "timestamp": "2023-06-26T11:28:39Z",
+          "url": "https://github.com/occlum/occlum/commit/e2f7e6109a3074e64730a950e62b1037d9e5e17c"
+        },
+        "date": 1688595518364,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 66.7,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 54.7,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 267,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 182,
             "unit": "MiB/s",
             "extra": "randread"
           }
