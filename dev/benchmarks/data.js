3cf43af3a879fdcc7bebfb330acad9a35f582597
github-action-benchmark <github@users.noreply.github.com>
2023-07-06 06:19:52 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for e2f7e6109a3074e64730a950e62b1037d9e5e17c

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688595520197,
+  "lastUpdate": 1688595592603,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -2730,6 +2730,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 262.64,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Shaowei Song",
+            "username": "lucassong-mh",
+            "email": "1498430017@qq.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "e2f7e6109a3074e64730a950e62b1037d9e5e17c",
+          "message": "[vm] Minor fix in debug asserts in shm",
+          "timestamp": "2023-06-26T11:28:39Z",
+          "url": "https://github.com/occlum/occlum/commit/e2f7e6109a3074e64730a950e62b1037d9e5e17c"
+        },
+        "date": 1688595590816,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.08,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 50.04,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 658.57,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 257.95,
             "unit": "ms",
             "extra": "per95"
           }
