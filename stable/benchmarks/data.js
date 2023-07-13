df7e4a743d61f33c02ffd0d939a505b44fa62b68
github-action-benchmark <github@users.noreply.github.com>
2023-07-13 11:11:16 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for 2772d9f7dbf7d12893b299bed1f9c4acc4bd7179

---
+++ stable/benchmarks/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1689217876444,
+  "repoUrl": "https://github.com/occlum/occlum",
+  "entries": {
+    "Iperf3 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "wang384670111",
+            "username": "wang384670111"
+          },
+          "committer": {
+            "name": "wang384670111",
+            "username": "wang384670111"
+          },
+          "id": "2772d9f7dbf7d12893b299bed1f9c4acc4bd7179",
+          "message": "Adjust the Ordering for all the atomics",
+          "timestamp": "2023-06-28T06:21:26Z",
+          "url": "https://github.com/occlum/occlum/pull/1334/commits/2772d9f7dbf7d12893b299bed1f9c4acc4bd7179"
+        },
+        "date": 1689217874888,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 819,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 819,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      }
+    ]
+  }
+}
