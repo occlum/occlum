69cb68ced31d97b95962321800aad0424789140d
ClawSeven <zehuan97@gmail.com>
2023-08-07 10:21:04 +0800

    Reduce the size of python-related demo for CI

--- demos/python/python_glibc/install_python_with_conda.sh
+++ demos/python/python_glibc/install_python_with_conda.sh
@@ -10,3 +10,6 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd )"
 [ -d miniconda ] || bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $script_dir/miniconda
 $script_dir/miniconda/bin/conda create --prefix $script_dir/python-occlum -y python=3.8.10 numpy=1.21.5 scipy=1.7.3 scikit-learn=1.0 pandas=1.3 Cython
 
+# 3. Remove miniconda and installation scripts
+rm -rf ./Miniconda3-latest-Linux-x86_64.sh $script_dir/miniconda
+
