5e679c24eeb29f2650a022064c42374de8c87350
github-action-benchmark <github@users.noreply.github.com>
2023-08-23 06:54:50 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 111230618ce6aea0c88a50c00eb5ad22c4211809

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692744883926,
+  "lastUpdate": 1692744890743,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -3374,6 +3374,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 248.83,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ClawSeven",
+            "username": "ClawSeven",
+            "email": "zehuan97@gmail.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "111230618ce6aea0c88a50c00eb5ad22c4211809",
+          "message": "Clean code coverage dependencies",
+          "timestamp": "2023-08-18T02:20:02Z",
+          "url": "https://github.com/occlum/occlum/commit/111230618ce6aea0c88a50c00eb5ad22c4211809"
+        },
+        "date": 1692744888910,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.08,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 84.4,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 16684.99,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 137.35,
             "unit": "ms",
             "extra": "per95"
           }
