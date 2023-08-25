80515ef6e2ca09313f1e1245de2c5fbc645a7362
github-action-benchmark <github@users.noreply.github.com>
2023-08-26 06:09:31 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 56f7ab02a56fa2863db4d390116c894f9f127dcb

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692744890743,
+  "lastUpdate": 1693001371598,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -3420,6 +3420,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 137.35,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "56f7ab02a56fa2863db4d390116c894f9f127dcb",
+          "message": "[vm] Refine VMA created with inherits_file_from",
+          "timestamp": "2023-08-24T03:09:43Z",
+          "url": "https://github.com/occlum/occlum/commit/56f7ab02a56fa2863db4d390116c894f9f127dcb"
+        },
+        "date": 1693001370574,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.08,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 48.06,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 602.36,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 244.38,
             "unit": "ms",
             "extra": "per95"
           }
