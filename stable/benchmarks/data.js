3f3d2fc0cb523441643f5e6377f1c416c8286af7
github-action-benchmark <github@users.noreply.github.com>
2023-08-31 06:13:55 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for 3225a378326f6ed939fd7886cd5aa566ba38fbd0

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1693088251411,
+  "lastUpdate": 1693433635512,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -536,6 +536,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 1950,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "3225a378326f6ed939fd7886cd5aa566ba38fbd0",
+          "message": "Remove adding process pid when creating multi-vma chunk",
+          "timestamp": "2023-08-29T08:55:05Z",
+          "url": "https://github.com/occlum/occlum/commit/3225a378326f6ed939fd7886cd5aa566ba38fbd0"
+        },
+        "date": 1693433633464,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 2012,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 2012,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
