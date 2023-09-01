8c73958ad449aaba48a4cb380e2c89893c54b0e6
github-action-benchmark <github@users.noreply.github.com>
2023-09-02 06:25:26 +0800

    add FIO Benchmark on SEFS (customBiggerIsBetter) benchmark result for 3225a378326f6ed939fd7886cd5aa566ba38fbd0

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1693607123139,
+  "lastUpdate": 1693607126489,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -8910,6 +8910,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 330,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "3225a378326f6ed939fd7886cd5aa566ba38fbd0",
+          "message": "Remove adding process pid when creating multi-vma chunk",
+          "timestamp": "2023-08-29T08:55:05Z",
+          "url": "https://github.com/occlum/occlum/commit/3225a378326f6ed939fd7886cd5aa566ba38fbd0"
+        },
+        "date": 1693607122097,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 150,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 87.9,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 373,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 295,
             "unit": "MiB/s",
             "extra": "randread"
           }
