565f191d6f2377022dc60ba40949e4deba243518
github-action-benchmark <github@users.noreply.github.com>
2023-10-11 06:29:51 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for ceb4a9871a2b683bc67b1e54d92058348f928b66

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1696976291655,
+  "lastUpdate": 1696976991653,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -4018,6 +4018,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 248.83,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Qi Zheng",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "ceb4a9871a2b683bc67b1e54d92058348f928b66",
+          "message": "[libos] uname machine should return x86_64",
+          "timestamp": "2023-10-08T07:12:52Z",
+          "url": "https://github.com/occlum/occlum/commit/ceb4a9871a2b683bc67b1e54d92058348f928b66"
+        },
+        "date": 1696976988859,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.08,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 88.99,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 24537.38,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 1.39,
             "unit": "ms",
             "extra": "per95"
           }
