1cf463908243c6ea3196f6d4f30337812f32ce33
github-action-benchmark <github@users.noreply.github.com>
2023-10-28 06:20:23 +0800

    add FIO Benchmark on SEFS (customBiggerIsBetter) benchmark result for 8759a57902d44f76c24424ca36e378eaa7f8b74c

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698444655360,
+  "lastUpdate": 1698445223969,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Sysbench Benchmark": [
@@ -10754,6 +10754,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 329,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "8759a57902d44f76c24424ca36e378eaa7f8b74c",
+          "message": "Fix concurrent mprotect/munmap causing conflict chunk and vma",
+          "timestamp": "2023-10-25T06:39:54Z",
+          "url": "https://github.com/occlum/occlum/commit/8759a57902d44f76c24424ca36e378eaa7f8b74c"
+        },
+        "date": 1698445222884,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 323,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 282,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 402,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 321,
             "unit": "MiB/s",
             "extra": "randread"
           }
