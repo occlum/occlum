c506334ca87582cfc4f40f44d4dcb7d64246addc
Qi Zheng <huaiqing.zq@antgroup.com>
2023-11-01 16:05:15 +0800

    [tools] Update ubuntu20.04-rt dockerfile for 0.30.0

--- tools/docker/docker-entrypoint.sh
+++ tools/docker/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Update PCCS_URL
-line=$(grep -n "pccs_url" /etc/sgx_default_qcnl.conf | cut -d ":" -f 1)
+line=$(grep -n '"pccs_url"' /etc/sgx_default_qcnl.conf | cut -d ":" -f 1)
 sed -i "${line}c \"pccs_url\": \"${PCCS_URL}\"," /etc/sgx_default_qcnl.conf
+# Update use_secure_cert
+line=$(grep -n '"use_secure_cert"' /etc/sgx_default_qcnl.conf | cut -d ":" -f 1)
+sed -i "${line}c \"use_secure_cert\": ${USE_SECURE_CERT}," /etc/sgx_default_qcnl.conf
 
 exec "$@"
