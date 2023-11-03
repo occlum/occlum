53f9cbe4adb6cb43b0189632d89dabdabbefdc6c
Qi Zheng <huaiqing.zq@antgroup.com>
2023-11-03 15:32:27 +0800

    [docker] Fix entrypoint typo for runtime docker image

--- tools/docker/docker-entrypoint.sh
+++ tools/docker/docker-entrypoint.sh
@@ -5,6 +5,6 @@ line=$(grep -n '"pccs_url"' /etc/sgx_default_qcnl.conf | cut -d ":" -f 1)
 sed -i "${line}c \"pccs_url\": \"${PCCS_URL}\"," /etc/sgx_default_qcnl.conf
 # Update use_secure_cert
 line=$(grep -n '"use_secure_cert"' /etc/sgx_default_qcnl.conf | cut -d ":" -f 1)
-sed -i "${line}c \"use_secure_cert\": ${USE_SECURE_CERT}," /etc/sgx_default_qcnl.conf
+sed -i "${line}c \"use_secure_cert\": ${USE_SECURE_CERT}" /etc/sgx_default_qcnl.conf
 
 exec "$@"
