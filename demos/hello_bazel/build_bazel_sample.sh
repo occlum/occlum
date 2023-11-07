b72922c316742c583d25a184211a46967c970a02
Hui, Chunyang <sanqian.hcy@antfin.com>
2023-11-07 03:47:11 +0000

    Fix bazel CI failure

--- demos/hello_bazel/build_bazel_sample.sh
+++ demos/hello_bazel/build_bazel_sample.sh
@@ -4,6 +4,7 @@ set -e
 mkdir examples
 cd examples
 git clone https://github.com/bazelbuild/examples/ .
+git checkout 93564e1f1e7a3c39d6a94acee12b8d7b74de3491
 cd cpp-tutorial/stage3
 export CC=/opt/occlum/toolchains/gcc/bin/occlum-gcc
 export CXX=/opt/occlum/toolchains/gcc/bin/occlum-g++
