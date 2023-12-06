251c023c0a8d0b9db87f6169428797edc2a6a9d9
ClawSeven <zehuan97@gmail.com>
2023-12-06 17:20:28 +0800

    Change netty ut java version  to jdk8

--- demos/netty_ut/preinstall_deps.sh
+++ demos/netty_ut/preinstall_deps.sh
@@ -3,8 +3,7 @@ set -e
 
 OS=`awk -F= '/^NAME/{print $2}' /etc/os-release`
 if [ "$OS" == "\"Ubuntu\"" ]; then
-  apt-get update -y && apt-get install -y openjdk-11-jdk
-  rm -rf /usr/lib/jvm/java-11-openjdk-amd64/lib/security/blacklisted.certs
+  apt-get update -y && apt-get install -y openjdk-8-jdk
 else
   echo "Unsupported OS: $OS"
   exit 1
