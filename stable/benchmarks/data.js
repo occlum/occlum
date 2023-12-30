531d26168ca379d5f8e4a1759e7f7e121edf29c0
github-action-benchmark <github@users.noreply.github.com>
2023-12-31 06:11:07 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for 9ee0a0ae73c109fb862d38e17e34ddf2cca792b8

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1703974265617,
+  "lastUpdate": 1703974267620,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -1690,6 +1690,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 2266,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "9ee0a0ae73c109fb862d38e17e34ddf2cca792b8",
+          "message": "Refine replace_mem_chunks to lock once",
+          "timestamp": "2023-12-22T09:46:00Z",
+          "url": "https://github.com/occlum/occlum/commit/9ee0a0ae73c109fb862d38e17e34ddf2cca792b8"
+        },
+        "date": 1703974265238,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 2291,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 2291,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
