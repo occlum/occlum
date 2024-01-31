b9b9c3518e311e767c12353466b9917d391c8fe7
github-action-benchmark <github@users.noreply.github.com>
2024-02-01 06:12:48 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 1c8ae036c2956b7b8a9c0299c2a34488505e8e97

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706393604316,
+  "lastUpdate": 1706739168058,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -4866,6 +4866,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Threads Maximum Latency",
             "value": 1828.01,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 569.67,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "jeffery.wsj",
+            "username": "superajun-wsj",
+            "email": "jeffery.wsj@alibaba-inc.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "1c8ae036c2956b7b8a9c0299c2a34488505e8e97",
+          "message": "Fix go exec demo ci bug v2",
+          "timestamp": "2024-01-11T06:59:05Z",
+          "url": "https://github.com/occlum/occlum/commit/1c8ae036c2956b7b8a9c0299c2a34488505e8e97"
+        },
+        "date": 1706739165967,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.55,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 90.95,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 1615.4,
             "unit": "ms",
             "extra": "max"
           },
