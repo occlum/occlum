2ca00288fd0564e86eb6ecc89ae33223967eb7c1
github-action-benchmark <github@users.noreply.github.com>
2024-02-11 06:13:28 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for 1c8ae036c2956b7b8a9c0299c2a34488505e8e97

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707343992368,
+  "lastUpdate": 1707603208310,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -2130,6 +2130,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 2298,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "jeffery.wsj",
+            "username": "superajun-wsj",
+            "email": "jeffery.wsj@alibaba-inc.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "1c8ae036c2956b7b8a9c0299c2a34488505e8e97",
+          "message": "Fix go exec demo ci bug v2",
+          "timestamp": "2024-01-11T06:59:05Z",
+          "url": "https://github.com/occlum/occlum/commit/1c8ae036c2956b7b8a9c0299c2a34488505e8e97"
+        },
+        "date": 1707603207194,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 2276,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 2276,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
