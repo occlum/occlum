8a7f4d64ca591552aae50c907f86e9fe50bd8e69
github-action-benchmark <github@users.noreply.github.com>
2024-02-15 06:13:40 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for 1c8ae036c2956b7b8a9c0299c2a34488505e8e97

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707948810868,
+  "lastUpdate": 1707948820521,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -5226,6 +5226,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 559.5,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "jeffery.wsj",
+            "username": "superajun-wsj",
+            "email": "jeffery.wsj@alibaba-inc.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "1c8ae036c2956b7b8a9c0299c2a34488505e8e97",
+          "message": "Fix go exec demo ci bug v2",
+          "timestamp": "2024-01-11T06:59:05Z",
+          "url": "https://github.com/occlum/occlum/commit/1c8ae036c2956b7b8a9c0299c2a34488505e8e97"
+        },
+        "date": 1707948818516,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.56,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 91.48,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 1673.99,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 569.67,
             "unit": "ms",
             "extra": "per95"
           }
