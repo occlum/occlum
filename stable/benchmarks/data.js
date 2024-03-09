75c6dfdc55b47af20ec1cc711c2fedf60e0e7bc4
github-action-benchmark <github@users.noreply.github.com>
2024-03-10 06:14:53 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for d60d603dbae364876a7cde96ace733cb7e62959d

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710022487766,
+  "lastUpdate": 1710022493936,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -5786,6 +5786,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 559.5,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Ikko Eltociear Ashimine",
+            "username": "eltociear",
+            "email": "eltociear@gmail.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "d60d603dbae364876a7cde96ace733cb7e62959d",
+          "message": "[readthedoc] Fix typo in init_ra.md\n\naquire -> acquire\r\naquired -> acquired",
+          "timestamp": "2024-03-03T15:19:00Z",
+          "url": "https://github.com/occlum/occlum/commit/d60d603dbae364876a7cde96ace733cb7e62959d"
+        },
+        "date": 1710022491932,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.55,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 91.93,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 1945.91,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 569.67,
             "unit": "ms",
             "extra": "per95"
           }
