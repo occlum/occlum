304616ed1579b435516ecb447eb898b1dc200c51
github-action-benchmark <github@users.noreply.github.com>
2024-04-18 06:11:41 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for 473eec584e66a9d2664879cc19240c15344227f6

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713391899095,
+  "lastUpdate": 1713391901141,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -2742,6 +2742,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 2235,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Qi Zheng",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "473eec584e66a9d2664879cc19240c15344227f6",
+          "message": "Update cargo lock",
+          "timestamp": "2024-03-19T01:38:23Z",
+          "url": "https://github.com/occlum/occlum/commit/473eec584e66a9d2664879cc19240c15344227f6"
+        },
+        "date": 1713391898623,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 2209,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 2209,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
