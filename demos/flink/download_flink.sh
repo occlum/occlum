ea04f97912e70e0628329bfc5032cd08ed80b277
Qi Zheng <huaiqing.zq@antgroup.com>
2024-04-28 15:52:23 +0800

    [demos] Update flink demo to version 1.15.2
    Also make both flink job manager and task manager run in Occlum

--- demos/flink/download_flink.sh
+++ demos/flink/download_flink.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-rm -rf flink-1.10.1*
-wget https://archive.apache.org/dist/flink/flink-1.10.1/flink-1.10.1-bin-scala_2.11.tgz
-tar -xvzf flink-1.10.1-bin-scala_2.11.tgz
+rm -rf flink-1.15.2*
+wget https://archive.apache.org/dist/flink/flink-1.15.2/flink-1.15.2-bin-scala_2.12.tgz
+tar -xvzf flink-1.15.2-bin-scala_2.12.tgz
 
 echo "Download Flink Success"
