6be5f827d400ef6bcebb4c7aaa001fec7790e3cf
ClawSeven <zehuan97@gmail.com>
2024-04-30 14:22:17 +0800

    [libos] Implement inner-defined sockopt with ioctl api

---
+++ src/libos/src/net/socket/sockopt/get_output.rs
@@ -0,0 +1,105 @@
+use super::{GetRecvTimeoutCmd, GetSendTimeoutCmd};
+
+use super::{
+    GetAcceptConnCmd, GetDomainCmd, GetErrorCmd, GetPeerNameCmd, GetRcvBufSizeCmd,
+    GetSndBufSizeCmd, GetSockOptRawCmd, GetTypeCmd,
+};
+
+use libc::timeval;
+use std::time::Duration;
+
+use crate::prelude::*;
+
+pub trait GetOutputAsBytes {
+    fn get_output_as_bytes(&self) -> Option<&[u8]>;
+}
+
+impl GetOutputAsBytes for GetSockOptRawCmd {
+    fn get_output_as_bytes(&self) -> Option<&[u8]> {
+        self.output()
+    }
+}
+
+impl GetOutputAsBytes for GetDomainCmd {
+    fn get_output_as_bytes(&self) -> Option<&[u8]> {
+        self.output().map(|val_ref| unsafe {
+            std::slice::from_raw_parts(val_ref as *const _ as *const u8, std::mem::size_of::<i32>())
+        })
+    }
+}
+
+impl GetOutputAsBytes for GetErrorCmd {
+    fn get_output_as_bytes(&self) -> Option<&[u8]> {
+        self.output().map(|val_ref| unsafe {
+            std::slice::from_raw_parts(val_ref as *const _ as *const u8, std::mem::size_of::<i32>())
+        })
+    }
+}
+
+impl GetOutputAsBytes for GetAcceptConnCmd {
+    fn get_output_as_bytes(&self) -> Option<&[u8]> {
+        self.output().map(|val_ref| unsafe {
+            std::slice::from_raw_parts(val_ref as *const _ as *const u8, std::mem::size_of::<i32>())
+        })
+    }
+}
+
+impl GetOutputAsBytes for GetPeerNameCmd {
+    fn get_output_as_bytes(&self) -> Option<&[u8]> {
+        self.output().map(|val_ref| unsafe {
+            std::slice::from_raw_parts(&(val_ref.0).0 as *const _ as *const u8, (val_ref.0).1)
+        })
+    }
+}
+
+impl GetOutputAsBytes for GetTypeCmd {
+    fn get_output_as_bytes(&self) -> Option<&[u8]> {
+        self.output().map(|val_ref| unsafe {
+            std::slice::from_raw_parts(val_ref as *const _ as *const u8, std::mem::size_of::<i32>())
+        })
+    }
+}
+
+impl GetOutputAsBytes for GetSndBufSizeCmd {
+    fn get_output_as_bytes(&self) -> Option<&[u8]> {
+        self.output().map(|val_ref| unsafe {
+            std::slice::from_raw_parts(
+                val_ref as *const _ as *const u8,
+                std::mem::size_of::<usize>(),
+            )
+        })
+    }
+}
+
+impl GetOutputAsBytes for GetRcvBufSizeCmd {
+    fn get_output_as_bytes(&self) -> Option<&[u8]> {
+        self.output().map(|val_ref| unsafe {
+            std::slice::from_raw_parts(
+                val_ref as *const _ as *const u8,
+                std::mem::size_of::<usize>(),
+            )
+        })
+    }
+}
+
+impl GetOutputAsBytes for GetRecvTimeoutCmd {
+    fn get_output_as_bytes(&self) -> Option<&[u8]> {
+        self.output().map(|val_ref| unsafe {
+            std::slice::from_raw_parts(
+                val_ref as *const _ as *const u8,
+                std::mem::size_of::<timeval>(),
+            )
+        })
+    }
+}
+
+impl GetOutputAsBytes for GetSendTimeoutCmd {
+    fn get_output_as_bytes(&self) -> Option<&[u8]> {
+        self.output().map(|val_ref| unsafe {
+            std::slice::from_raw_parts(
+                val_ref as *const _ as *const u8,
+                std::mem::size_of::<timeval>(),
+            )
+        })
+    }
+}
