afd69f8fe1eb12e6fed7cc895bb9ae0dc44edc45
github-action-benchmark <github@users.noreply.github.com>
2024-06-20 06:11:09 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for be4de47940ca6f9c3a9d4873587dfa592b2b2dc6

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718489429362,
+  "lastUpdate": 1718835069987,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -7986,6 +7986,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Threads Maximum Latency",
             "value": 1718.41,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 559.5,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ClawSeven",
+            "username": "ClawSeven",
+            "email": "zehuan97@gmail.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "be4de47940ca6f9c3a9d4873587dfa592b2b2dc6",
+          "message": "[Demos] Fix broken mnist source in paddlepaddle demo",
+          "timestamp": "2024-05-27T08:52:07Z",
+          "url": "https://github.com/occlum/occlum/commit/be4de47940ca6f9c3a9d4873587dfa592b2b2dc6"
+        },
+        "date": 1718835068843,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.57,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 91.01,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 1760.43,
             "unit": "ms",
             "extra": "max"
           },
