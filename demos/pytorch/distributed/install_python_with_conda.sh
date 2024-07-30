93e08150c41b854a358bd5300455d92b919244fe
ClawSeven <zehuan97@gmail.com>
2024-07-30 16:22:08 +0800

    [CI] Fix distributed pytorch and netty ut CI crash

--- demos/pytorch/distributed/install_python_with_conda.sh
+++ demos/pytorch/distributed/install_python_with_conda.sh
@@ -6,7 +6,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd )"
 [ -f Miniconda3-latest-Linux-x86_64.sh ] || wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 [ -d miniconda ] || bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $script_dir/miniconda
 $script_dir/miniconda/bin/conda create --prefix $script_dir/python-occlum -y \
-    python=3.8.10 numpy=1.21.5 scipy=1.7.3 scikit-learn=1.0 pandas=1.3 \
+    python=3.8.10 numpy=1.22.3 scipy=1.7.3 scikit-learn=1.0 pandas=1.3 \
     Cython pytorch torchvision -c pytorch
 
 # Remove miniconda and installation scripts
