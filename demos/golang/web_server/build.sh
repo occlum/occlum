ebb84d58d050aec22f54deb73a903a0687e4db4b
Zheng, Qi <huaiqing.zq@antgroup.com>
2024-09-06 14:47:22 +0800

    [demos] fix go webserver build failure

--- demos/golang/web_server/build.sh
+++ demos/golang/web_server/build.sh
@@ -4,7 +4,5 @@ set -e
 rm -f go.mod
 occlum-go mod init web_server
 occlum-go mod tidy
-occlum-go get -u -v github.com/gin-gonic/gin
-occlum-go get -u -v golang.org/x/crypto@v0.23.0
 
 occlum-go build -o web_server ./web_server.go
