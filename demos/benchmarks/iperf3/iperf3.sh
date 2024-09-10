09ed89de65e1b69a2b758c49983990f7f9eb9285
Zheng, Qi <huaiqing.zq@antgroup.com>
2024-09-10 09:57:15 +0800

    [demo] Add io_uring test mode for iperf3

--- demos/benchmarks/iperf3/iperf3.sh
+++ demos/benchmarks/iperf3/iperf3.sh
@@ -30,6 +30,28 @@ function iperf3_run()
     popd
 }
 
+function iperf3_iouring_run()
+{
+    echo ""
+    echo "*** Doing iperf3 io_uring with ${STREMS} client streams in parallel ***"
+    echo "*** with read/write buffer length ${BUF_LEN} for ${TEST_TIME} seconds. ***"
+
+    pushd occlum_server
+    jq '.feature.io_uring = 1' Occlum.json > /tmp.json && mv /tmp.json Occlum.json
+    occlum build -f
+    occlum run /bin/iperf3 -s -p 6777 -1 1>/dev/null &
+    popd
+
+    sleep 3
+
+    pushd occlum_client
+    jq '.feature.io_uring = 1' Occlum.json > /tmp.json && mv /tmp.json Occlum.json
+    occlum build -f
+    occlum run /bin/iperf3 -c 127.0.0.1 -p 6777 -f Mbits \
+        -P ${STREMS} -t ${TEST_TIME} -l ${BUF_LEN} | tee output_iouring.txt
+    popd
+}
+
 function iperf3_result()
 {
     output="occlum_client/output.txt"
@@ -40,8 +62,27 @@ function iperf3_result()
         '(.[] | select(.extra == "sender") | .value) |= $sender |
         (.[] | select(.extra == "receiver") | .value) |= $recv'  \
         result_template.json  > result.json
+
+    if [ "$IO_URING_TEST" == "true" ]; then
+        output="occlum_client/output_iouring.txt"
+        SENDER_RES=$(grep "SUM" ${output} | grep "sender" | awk '{print $6}')
+        RECV_RES=$(grep "SUM" ${output} | grep "receiver" | awk '{print $6}')
+
+        jq --argjson sender $SENDER_RES --argjson recv $RECV_RES \
+            '(.[] | select(.extra == "sender_iouring") | .value) |= $sender |
+            (.[] | select(.extra == "receiver_iouring") | .value) |= $recv'  \
+            result.json  > tmp.json
+
+        mv tmp.json result.json
+    fi
 }
 
 iperf3_prepare
 iperf3_run
+
+# Do io_uring mode test
+if [ "$IO_URING_TEST" == "true" ]; then
+iperf3_iouring_run
+fi
+
 iperf3_result
