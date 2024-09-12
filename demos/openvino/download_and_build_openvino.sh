e2f771c25bc63254d2756dabaf159b1d5ca6bdc1
Zheng, Qi <huaiqing.zq@antgroup.com>
2024-09-12 15:50:20 +0800

    [demos] Update openvino demo to glibc based

--- demos/openvino/download_and_build_openvino.sh
+++ demos/openvino/download_and_build_openvino.sh
@@ -1,109 +1,37 @@
 #!/bin/bash
-PREFIX=/usr/local/occlum/x86_64-linux-musl
-THREADING=TBB
 set -e
 
-show_usage() {
-    echo
-    echo "Usage: $0 [--threading <TBB/OMP>]"
-    echo
-    exit 1
-}
+export OPENVINO_DIR="openvino_src"
+OPENVINO_VERSION="2023.3.0"
+PREFIX="/usr/local/openvino"
 
-build_opencv() {
-    rm -rf deps/opencv && mkdir -p deps/opencv
-    pushd deps/opencv
-    git clone https://github.com/opencv/opencv .
-    git checkout tags/4.1.0 -b 4.1.0
-    mkdir build
-    cd build
-    cmake ../ \
-      -DCMAKE_BUILD_TYPE=RELEASE \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_C_COMPILER=occlum-gcc -DCMAKE_CXX_COMPILER=occlum-g++ \
-      -DENABLE_PRECOMPILED_HEADERS=OFF \
-      -DOPENCV_PC_FILE_NAME=opencv.pc \
-      -DOPENCV_GENERATE_PKGCONFIG=ON \
-      -DBUILD_opencv_java=OFF -DBUILD_JAVA_SUPPORT=OFF \
-      -DBUILD_opencv_python=OFF -DBUILD_PYTHON_SUPPORT=OFF \
-      -DBUILD_EXAMPLES=OFF -DWITH_FFMPEG=OFF \
-      -DWITH_QT=OFF -DWITH_CUDA=OFF
-    make -j$(nproc)
-    sudo make install
-    popd
-}
+download_and_build_openvino() {
+    rm -rf $OPENVINO_DIR
+    git clone https://github.com/openvinotoolkit/openvino.git $OPENVINO_DIR
+    pushd $OPENVINO_DIR
+    git checkout -b $OPENVINO_VERSION $OPENVINO_VERSION
+    git submodule update --init --recursive
+    ./scripts/submodule_update_with_gitee.sh
 
-# Build oneTBB,  OpenVINO_2021 need version 2020.3
-build_tbb() {
-    rm -rf deps/tbb_cmake && mkdir -p deps/tbb_cmake
-    pushd deps/tbb_cmake
-    git clone https://github.com/oneapi-src/oneTBB.git .
-    git checkout v2020.3
-    CXX=occlum-g++ CC=occlum-gcc make tbb -j4
-    find build/ -name libtbb* -exec cp {} $PREFIX/lib/ \;
-    popd
-}
-
-
-# Build OpenVINO
-build_openvino() {
-    rm -rf openvino_src && mkdir openvino_src
-    pushd openvino_src
-    git clone https://github.com/opencv/dldt .
-    git checkout 2021.3
-    git submodule init
-    git submodule update --recursive
     mkdir build && cd build
-#   Substitute THREADING lib
-    cmake ../ \
-      -DTHREADING=$THREADING \
-      -DENABLE_MKL_DNN=ON \
-      -DENABLE_CLDNN=OFF \
-      -DENABLE_MYRIAD=OFF \
-      -DENABLE_GNA=OFF
-    [ "$THREADING" == "OMP" ] && rm -rf ../inference-engine/temp/omp/lib/* && cp $PREFIX/lib/libgomp.so ../inference-engine/temp/omp/lib/libiomp5.so
-    [ "$THREADING" == "TBB" ] && rm -rf ../inference-engine/temp/tbb/lib/* && cp $PREFIX/lib/libtbb.so.2 ../inference-engine/temp/tbb/lib
-    cd ../
-    rm -rf build && mkdir build && cd build
-#   Substitute OpenCV
-    export OpenCV_DIR=$PREFIX/lib/cmake/opencv4
-    cmake ../ \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_CXX_COMPILER=occlum-g++ -DCMAKE_CXX_FLAGS="-Wno-error=stringop-overflow=" \
-      -DCMAKE_C_COMPILER=occlum-gcc -DCMAKE_C_FLAGS="-Wno-error=stringop-overflow=" \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DTHREADING=$THREADING \
-      -DENABLE_MKL_DNN=ON \
-      -DENABLE_CLDNN=OFF \
-      -DENABLE_OPENCV=OFF \
-      -DENABLE_MYRIAD=OFF \
-      -DENABLE_GNA=OFF \
-      -DENABLE_VPU=OFF
-    make -j$(nproc)
+    cmake ../ -DENABLE_INTEL_GPU=OFF \
+        -DENABLE_INTEL_GNA=OFF \
+        -DENABLE_HETERO=OFF \
+        -DENABLE_INTEL_NPU=OFF \
+        -DTHREADING=OMP \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DCMAKE_BUILD_TYPE=Release
+    make --jobs=$(nproc --all)
+    make install
     popd
 }
 
-while [ -n "$1" ]; do
-    case "$1" in
-    --threading)    [ -n "$2" ] && THREADING=$2 ; shift 2 || show_usage ;;
-    *)
-        show_usage
-    esac
-done
+# build benchmark_app
+build_sample() {
+    pushd $PREFIX/samples/cpp
+    ./build_samples.sh -b benchmark_app
+    popd
+}
 
-# Tell CMake to search for packages in Occlum toolchain's directory only
-export PKG_CONFIG_LIBDIR=$PREFIX/lib
-
-if [ "$THREADING" == "TBB" ] ; then
-    echo "Build OpenVINO with TBB threading"
-    build_opencv
-    build_tbb
-    build_openvino
-elif [ "$THREADING" == "OMP" ] ; then
-    echo "Build OpenVINO with OpenMP threading"
-    build_opencv
-    build_openvino
-else
-    echo "Error: invalid threading: $THREADING"
-    show_usage
-fi
+download_and_build_openvino
+build_sample
--- demos/openvino/run_benchmark_on_occlum.sh
+++ demos/openvino/run_benchmark_on_occlum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-benchmark=benchmark_app
 set -e
 
+benchmark=benchmark_app
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 bomfile=${SCRIPT_DIR}/openvino.yaml
 
@@ -10,21 +10,16 @@ rm -rf occlum_instance
 mkdir occlum_instance
 cd occlum_instance
 occlum init
-cpu_cc=`cat /proc/cpuinfo | grep processor | wc -l`
-#new_json="$(jq '.resource_limits.user_space_size = "4GB" |
-#                .resource_limits.kernel_space_heap_size = "128MB" |
-#                .resource_limits.kernel_space_stack_size = "16MB" |
-#                .resource_limits.max_num_of_threads = 128 |
-#                .process.default_stack_size = "8MB" |
-#                .process.default_heap_size = "32MB" |
-#                .metadata.debuggable = false ' Occlum.json)" && \
 new_json="$(jq '.resource_limits.max_num_of_threads = 64 |
                 .resource_limits.user_space_size = "1MB" |
-                .resource_limits.user_space_max_size = "600MB"' Occlum.json)" && \
+                .resource_limits.user_space_max_size = "2GB" |
+                .env.default = [ "LD_LIBRARY_PATH=/usr/local/openvino/runtime/lib/intel64" ] ' Occlum.json)" && \
 echo "${new_json}" > Occlum.json
 
 # 2. Copy files into Occlum Workspace and Build
 rm -rf image
+# make sure libiomp5.so could be found by copy_bom
+cp /usr/local/openvino/runtime/lib/intel64/libiomp5.so /usr/local/lib/
 copy_bom -f $bomfile --root image --include-dir /opt/occlum/etc/template
 
 occlum build
