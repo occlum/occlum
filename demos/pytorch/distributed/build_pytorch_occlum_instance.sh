bdf8c1db8cb258474a4f111f1650d77b700f2ac4
Zheng, Qi <huaiqing.zq@antgroup.com>
2024-10-24 11:40:26 +0800

    [demos] Increase user space size for pytorch demo

--- demos/pytorch/distributed/build_pytorch_occlum_instance.sh
+++ demos/pytorch/distributed/build_pytorch_occlum_instance.sh
@@ -35,7 +35,7 @@ function build_instance()
     fi
 
     new_json="$(jq '.resource_limits.user_space_size = "1MB" |
-                    .resource_limits.user_space_max_size = "4000MB" |
+                    .resource_limits.user_space_max_size = "5000MB" |
                     .resource_limits.kernel_space_heap_size = "1MB" |
                     .resource_limits.kernel_space_heap_max_size = "400MB" |
                     .resource_limits.max_num_of_threads = 64 |
