53ef0b7862389712b6fd510fe3ade1b9747e5dac
github-action-benchmark <github@users.noreply.github.com>
2024-12-19 06:10:44 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for be4de47940ca6f9c3a9d4873587dfa592b2b2dc6

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734559828040,
+  "lastUpdate": 1734559844929,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -11386,6 +11386,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 549.52,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ClawSeven",
+            "username": "ClawSeven",
+            "email": "zehuan97@gmail.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "be4de47940ca6f9c3a9d4873587dfa592b2b2dc6",
+          "message": "[Demos] Fix broken mnist source in paddlepaddle demo",
+          "timestamp": "2024-05-27T08:52:07Z",
+          "url": "https://github.com/occlum/occlum/commit/be4de47940ca6f9c3a9d4873587dfa592b2b2dc6"
+        },
+        "date": 1734559842780,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.55,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 87.7,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 1816.66,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 539.71,
             "unit": "ms",
             "extra": "per95"
           }
