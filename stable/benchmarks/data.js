4e7c2e52f7467d3cc4f15967053d8ef5fca7755a
github-action-benchmark <github@users.noreply.github.com>
2025-03-22 10:12:32 +0800

    add Fio Benchmark (customBiggerIsBetter) benchmark result for d88f6e36301ae77e5c08f919f781ab3c9b0cce72

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742422753641,
+  "lastUpdate": 1742609552841,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -13804,6 +13804,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 223,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Qi Zheng",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "d88f6e36301ae77e5c08f919f781ab3c9b0cce72",
+          "message": "[ci] Use non test docker image to generate debian package",
+          "timestamp": "2025-03-10T05:58:38Z",
+          "url": "https://github.com/occlum/occlum/commit/d88f6e36301ae77e5c08f919f781ab3c9b0cce72"
+        },
+        "date": 1742609551643,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 51,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 51.4,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 198,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 880,
             "unit": "MiB/s",
             "extra": "randread"
           }
