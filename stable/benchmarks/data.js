f99c570a60dd85fdb3cff60de48ef5fc92c5a47f
github-action-benchmark <github@users.noreply.github.com>
2025-04-06 06:13:36 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for d88f6e36301ae77e5c08f919f781ab3c9b0cce72

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743680249993,
+  "lastUpdate": 1743891216481,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -5944,6 +5944,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate (IO_URING)",
             "value": 4751,
+            "unit": "Mbits/sec",
+            "extra": "receiver_iouring"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Qi Zheng",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "d88f6e36301ae77e5c08f919f781ab3c9b0cce72",
+          "message": "[ci] Use non test docker image to generate debian package",
+          "timestamp": "2025-03-10T05:58:38Z",
+          "url": "https://github.com/occlum/occlum/commit/d88f6e36301ae77e5c08f919f781ab3c9b0cce72"
+        },
+        "date": 1743891213617,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 2601,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 2601,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          },
+          {
+            "name": "Sender Average Rate (IO_URING)",
+            "value": 4740,
+            "unit": "Mbits/sec",
+            "extra": "sender_iouring"
+          },
+          {
+            "name": "Receiver Average Rate (IO_URING)",
+            "value": 4740,
             "unit": "Mbits/sec",
             "extra": "receiver_iouring"
           }
