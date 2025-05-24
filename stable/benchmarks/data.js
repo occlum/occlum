b8ebeef74bde61de71aba318c14c0f968d27f0b8
github-action-benchmark <github@users.noreply.github.com>
2025-05-25 06:28:35 +0800

    add Fio Benchmark (customBiggerIsBetter) benchmark result for 7fc069627c8c1995b6fc50fa85c3731e6f9ccbad

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748125000221,
+  "lastUpdate": 1748125715874,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -15690,6 +15690,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 203,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "7fc069627c8c1995b6fc50fa85c3731e6f9ccbad",
+          "message": "[doc] Update Github Action status",
+          "timestamp": "2025-04-28T03:02:31Z",
+          "url": "https://github.com/occlum/occlum/commit/7fc069627c8c1995b6fc50fa85c3731e6f9ccbad"
+        },
+        "date": 1748125712880,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 50.7,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 42.5,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 154,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 143,
             "unit": "MiB/s",
             "extra": "randread"
           }
