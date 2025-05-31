7c91104324a43f31a158cbcefa760867d60cbfff
github-action-benchmark <github@users.noreply.github.com>
2025-06-01 06:17:15 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for 7fc069627c8c1995b6fc50fa85c3731e6f9ccbad

--- stable/benchmarks/data.js
+++ stable/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748125715874,
+  "lastUpdate": 1748729835250,
   "repoUrl": "https://github.com/occlum/occlum",
   "entries": {
     "Iperf3 Benchmark": [
@@ -6358,6 +6358,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate (IO_URING)",
             "value": 4028,
+            "unit": "Mbits/sec",
+            "extra": "receiver_iouring"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Hui, Chunyang",
+            "username": "jessehui",
+            "email": "sanqian.hcy@antfin.com"
+          },
+          "committer": {
+            "name": "volcano",
+            "username": "volcano0dr",
+            "email": "volcano.dr@hotmail.com"
+          },
+          "id": "7fc069627c8c1995b6fc50fa85c3731e6f9ccbad",
+          "message": "[doc] Update Github Action status",
+          "timestamp": "2025-04-28T03:02:31Z",
+          "url": "https://github.com/occlum/occlum/commit/7fc069627c8c1995b6fc50fa85c3731e6f9ccbad"
+        },
+        "date": 1748729833817,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 2577,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 2577,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          },
+          {
+            "name": "Sender Average Rate (IO_URING)",
+            "value": 4802,
+            "unit": "Mbits/sec",
+            "extra": "sender_iouring"
+          },
+          {
+            "name": "Receiver Average Rate (IO_URING)",
+            "value": 4802,
             "unit": "Mbits/sec",
             "extra": "receiver_iouring"
           }
